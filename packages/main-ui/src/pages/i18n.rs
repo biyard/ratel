2c958f41ec463b5db46a60e9fa8d1acc958e2b94
hackartists <hackartists@gmail.com>
2024-12-18 14:57:56 +0900

    refactor(i18n): rename text field to _text in PagesTranslate struct

--- packages/main-ui/src/pages/i18n.rs
+++ packages/main-ui/src/pages/i18n.rs
@@ -1,13 +1,13 @@
 use crate::route::Language;
 
 pub struct PagesTranslate<'a> {
-    pub text: &'a str,
+    pub _text: &'a str,
 }
 
 pub fn translate_pages<'a>(lang: &'a Language) -> PagesTranslate<'a> {
     if lang.clone() == Language::Ko {
         PagesTranslate {
-            text: "homepage page",
+            _text: "homepage page",
         }
     } else {
         todo!()
