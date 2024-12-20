0bac82a3fe6c10c179174cf4351aadd093647b26
hackartists <hackartists@gmail.com>
2024-12-20 19:15:29 +0900

    fix(ui): adjust spacing and layout in HomePage component

--- packages/main-ui/src/pages/page.rs
+++ packages/main-ui/src/pages/page.rs
@@ -16,14 +16,14 @@ pub fn HomePage(lang: Language) -> Element {
 
     rsx! {
         div {
-            class: "flex flex-col gap-[100px] grid grid-cols-1",
+            class: "flex flex-col gap-[100px] grid grid-cols-1 mb-[20px]",
             HighlightedTopics {
                 class: "col-span-1",
                 topics: ctrl.ongoing_topics(),
                 onselect: |_| {},
             }
             div {
-                class: "col-span-1 w-full flex flex-row items-start justify-center gap-[20px] grid grid-cols-2 max-[1000px]:grid-cols-1",
+                class: "col-span-1 w-full flex flex-row items-start justify-center gap-x-[20px] gap-y-[60px] grid grid-cols-2 max-[1000px]:grid-cols-1",
                 FinishedTopics {
                     class: "col-span-1",
                     _topics: ctrl.finished_topics(),
