6eda966f2fa9e2aec7f65f2be580472e43a56d7c
2miwon <zhengsfsf@gmail.com>
2025-01-06 21:43:29 +0900

    remove non-use variable

--- packages/main-api/src/models/openapi/member.rs
+++ packages/main-api/src/models/openapi/member.rs
@@ -2,84 +2,12 @@
 pub struct Member {
     #[serde(rename(deserialize = "HG_NM"))]
     name: String, // 한글 이름
-    #[serde(skip_serializing)]
-    #[serde(skip_deserializing)]
-    #[allow(dead_code)]
-    hj_nm: String, // 한자 이름
     #[serde(rename(deserialize = "ENG_NM"))]
     eng_name: String, // 영문 이름
-    #[serde(skip_serializing)]
-    #[serde(skip_deserializing)]
-    #[allow(dead_code)]
-    bth_gbn_nm: String,
-    #[serde(skip_serializing)]
-    #[serde(skip_deserializing)]
-    #[allow(dead_code)]
-    bth_date: String,
-    #[serde(skip_serializing)]
-    #[serde(skip_deserializing)]
-    #[allow(dead_code)]
-    job_res_nm: String,
     #[serde(rename(deserialize = "POLY_NM"))]
     party: String, // 정당명
     #[serde(rename(deserialize = "ORIG_NM"))]
-    district: String, // 선거구명    
-    #[serde(skip_serializing)]
-    #[serde(skip_deserializing)]
-    #[allow(dead_code)]
-    elect_gbn_nm: String, // 지역구 or 비례대표
-    #[serde(skip_serializing)]
-    #[serde(skip_deserializing)]
-    #[allow(dead_code)]
-    cmit_nm: String, // 대표 위원회
-    #[serde(skip_serializing)]
-    #[serde(skip_deserializing)]
-    #[allow(dead_code)]
-    cmits: String, // 소속 위원회 목록
-    #[serde(skip_serializing)]
-    #[serde(skip_deserializing)]
-    #[allow(dead_code)]
-    reele_gbn_nm: String, // 재선
-    #[serde(skip_serializing)]
-    #[serde(skip_deserializing)]
-    #[allow(dead_code)]
-    units: String,
-    #[serde(skip_serializing)]
-    #[serde(skip_deserializing)]
-    #[allow(dead_code)]
-    sex_gbn_nm: String,
-    #[serde(skip_serializing)]
-    #[serde(skip_deserializing)]
-    #[allow(dead_code)]
-    tel_no: String,
-    #[serde(skip_serializing)]
-    #[serde(skip_deserializing)]
-    #[allow(dead_code)]
-    e_mail: String,
-    #[serde(skip_serializing)]
-    #[serde(skip_deserializing)]
-    #[allow(dead_code)]
-    homepage: Option<String>,
-    #[serde(skip_serializing)]
-    #[serde(skip_deserializing)]
-    #[allow(dead_code)]
-    staff: String,
-    #[serde(skip_serializing)]
-    #[serde(skip_deserializing)]
-    #[allow(dead_code)]
-    secretary: String,
-    #[serde(skip_serializing)]
-    #[serde(skip_deserializing)]
-    #[allow(dead_code)]
-    secretary2: String,
+    district: String, // 선거구명
     #[serde(rename(deserialize = "MONA_CD"))]
     code: String, // 고유식별번호
-    #[serde(skip_serializing)]
-    #[serde(skip_deserializing)]
-    #[allow(dead_code)]
-    mem_title: String,
-    #[serde(skip_serializing)]
-    #[serde(skip_deserializing)]
-    #[allow(dead_code)]
-    assem_addr: String,
 }
