d5b194dc64cbbd83dd31c6be68dc24a5f1559291
Jace0615 <jace@biyard.co>
2025-02-03 10:34:27 +0900

    Remove .DS_Store

---
+++ packages/main-ui/src/components/footer/i18n.rs
@@ -0,0 +1,19 @@
+use dioxus_translate::translate;
+
+translate! {
+    FooterTranslate;
+
+    title_text: {
+        ko: "Mine the Future, Cast Your Predictions.",
+        en: "Mine the Future, Cast Your Predictions.",
+    },
+
+    privacy_policy_button_text: {
+        ko: "개인 정보 보호 정책",
+        en: "Privacy Policy",
+    },
+    terms_of_service_button_text: {
+        ko: "서비스 약관",
+        en: "Terms of Service",
+    },
+}
