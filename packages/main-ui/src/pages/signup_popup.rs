e2a8de19d698b8decbf2f82c3266c44fc5adf5ea
2miwon <zhengsfsf@gmail.com>
2025-02-18 00:22:39 +0900

    user wallet_adapter crate

--- packages/main-ui/src/pages/signup_popup.rs
+++ packages/main-ui/src/pages/signup_popup.rs
@@ -12,7 +12,6 @@ use dioxus_translate::*;
 pub fn SignupPopup(
     #[props(default ="signup_popup".to_string())] id: String,
     #[props(default ="".to_string())] class: String,
-    // onclick: EventHandler<Event<MouseData>>,
     lang: Language,
 ) -> Element {
     let tr = translate::<SignupPopupTranslate>(&lang);
@@ -25,7 +24,7 @@ pub fn SignupPopup(
                 class: "w-full flex flex-row my-[10px] p-[8px] bg-[#6D7AFF] rounded-[8px] justify-start items-center gap-[17px] cursor-pointer hover:bg-[#5C6BFF]",
                 onclick: move |_| async move {
                     tracing::debug!("Signup with Google clicked");
-                    match user_service.login().await {
+                    match user_service.google_login().await {
                         UserEvent::Signup(principal, email, nickname, profile_url) => {
                             popup.open(rsx! {
                                 UserSetupPopup {
@@ -59,21 +58,44 @@ pub fn SignupPopup(
                     }
                 }
             }
-                // div {
-        //     class: "w-full flex flex-row my-[10px] p-[8px] bg-[#AB9FF1] rounded-[8px] justify-start items-center gap-[17px] cursor-pointer hover:bg-[#9A8EFF]",
-        //     onclick,
-        //     div { class: "rounded-[8px] w-[62px] h-[62px] flex items-center justify-center",
-        //         icons::Phantom {}
-        //     }
-        //     div { class: "flex flex-col gap-[3px]",
-        //         span { class: "text-white text-[16px] leading-[16px] font-extrabold",
-        //             "{tr.continue_with_phantom_wallet}"
-        //         }
-        //         span { class: "text-white text-[14px] leading-[13px] fond-regular",
-        //             "{tr.connect_wallet}"
-        //         }
-        //     }
-        // }
+            div {
+                class: "w-full flex flex-row my-[10px] p-[8px] bg-[#AB9FF1] rounded-[8px] justify-start items-center gap-[17px] cursor-pointer hover:bg-[#9A8EFF]",
+                onclick: move |_| async move {
+                    tracing::debug!("Signup with Phantom clicked");
+                    match user_service.phantom_login().await {
+                        UserEvent::Signup(principal, email, nickname, profile_url) => {
+                            popup.open(rsx! {
+                                UserSetupPopup {
+                                    class: "w-[400px]",
+                                    nickname,
+                                    profile_url,
+                                    email,
+                                    principal,
+                                    lang: lang.clone(),
+                                }
+                            });
+                        }
+                        UserEvent::Login => {
+                            popup.close();
+                        }
+                        _ => {
+                            tracing::error!("Failed to signup with Phantom");
+                            popup.close();
+                        }
+                    };
+                },
+                div { class: "rounded-[8px] w-[62px] h-[62px] flex items-center justify-center",
+                    icons::Phantom {}
+                }
+                div { class: "flex flex-col gap-[3px]",
+                    span { class: "text-white text-[16px] leading-[16px] font-extrabold",
+                        "{tr.continue_with_phantom_wallet}"
+                    }
+                    span { class: "text-white text-[14px] leading-[13px] fond-regular",
+                        "{tr.connect_wallet}"
+                    }
+                }
+            }
         }
     }
 }
