c0533af2349d3eabe1f5c2448583d1bfc7791b8e
2miwon <zhengsfsf@gmail.com>
2025-02-25 16:29:04 +0900

    feat topic test

--- packages/main-api/src/controllers/v1/topics/tests.rs
+++ packages/main-api/src/controllers/v1/topics/tests.rs
@@ -1,24 +1,19 @@
 use super::*;
-use crate::tests::{setup, TestContext};
-use dto::*;
+use crate::tests::setup;
+// use dto::*;
 
 #[tokio::test]
 async fn test_topic() {
-    let context = TestContext {
-        pool,
-        user,
-        endpoint,
-        ..
-    } = setup().await.unwrap();
+    let context = setup().await.unwrap();
 
-    let cli_topic = Topic::get_client(&endpoint);
+    let cli_topic = Topic::get_client(&context.endpoint);
 
-    test_create_topic(context.clone(), cli_topic.clone());
+    let _id = test_create_topic(context.now, cli_topic.clone()).await;
 }
 
-async fn test_create_topic(context: TestContext, cli: TopicClient) {
-    let ended_at = context.now + 3600;
-    let topic = cli
+async fn test_create_topic(now: i64, cli: TopicClient) -> i64 {
+    let ended_at = now + 3600;
+    let res = cli
         .create(
             ended_at,
             "test title".to_string(),
@@ -33,24 +28,19 @@ async fn test_create_topic(context: TestContext, cli: TopicClient) {
             vec![
                 AdditionalResource {
                     filename: "test additional_resources1".to_string(),
-                    extension: "pdf".to_string(),
+                    extension: FileType::Pdf,
                     link: "https://test.com".to_string(),
                 },
                 AdditionalResource {
                     filename: "test additional_resources2".to_string(),
-                    extension: "pdf".to_string(),
+                    extension: FileType::Image,
                     link: "https://test.com".to_string(),
                 },
             ],
         )
-        .await
-        .unwrap();
+        .await;
 
-    assert_eq!(topic.title, "test title");
-    assert_eq!(topic.content, "test content");
-    assert_eq!(topic.status, TopicStatus::Scheduled);
-    assert_eq!(topic.legislation_link, "https://test.com");
-    assert_eq!(topic.solutions, "test solutions");
-    assert_eq!(topic.discussions.len(), 2);
-    assert_eq!(topic.additional_resources.len(), 2);
+    tracing::debug!("test_create_topic: {:?}", res);
+    assert!(res.is_ok(), "create topic failed: {:?}", res);
+    res.unwrap().id
 }
