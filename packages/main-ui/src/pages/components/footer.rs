a660a0dc611290ed0866429d261c9587ed7897a7
hackartists <hackartists@gmail.com>
2025-03-09 20:01:24 +0900

    feat(footer): update footer layout with new SVG and structure

--- packages/main-ui/src/pages/components/footer.rs
+++ packages/main-ui/src/pages/components/footer.rs
@@ -9,30 +9,44 @@ pub fn Footer(lang: Language) -> Element {
     let tr: FooterTranslate = translate(&lang);
 
     rsx! {
-        footer { class: "w-full flex flex-col items-center justify-center h-367 pt-80 pb-24 bg-footer gap-80",
-            div { class: "w-full flex max-w-604 flex-col items-center gap-80",
+        div { class: "w-full flex flex-col",
+            div { class: "w-full",
+                svg {
+                    fill: "none",
+                    view_box: "0 0 1921 146",
+                    width: "100%",
+                    xmlns: "http://www.w3.org/2000/svg",
+                    path {
+                        d: "M0.25 73.7684L1920.25 0V146H0.25V73.7684Z",
+                        fill: "#191919",
+                    }
+                }
+            }
+            footer { class: "w-full flex flex-col items-center justify-center h-367 pt-80 pb-24 bg-footer gap-80",
+                div { class: "w-full flex max-w-604 flex-col items-center gap-80",
 
-                div { class: "w-full flex flex-col gap-50 items-center",
-                    div { class: "w-full flex flex-col items-start gap-5",
-                        p { class: "text-c-cg-30 font-bold text-base/28", {tr.email} }
-                        InputWithButton {
-                            placeholder: tr.email_placeholder,
-                            btn_name: tr.btn_subscribe,
-                            r#type: "email",
-                            onsubmit: |email| {
-                                btracing::info!("Subscribed by {}", email);
-                            },
+                    div { class: "w-full flex flex-col gap-50 items-center",
+                        div { class: "w-full flex flex-col items-start gap-5",
+                            p { class: "text-c-cg-30 font-bold text-base/28", {tr.email} }
+                            InputWithButton {
+                                placeholder: tr.email_placeholder,
+                                btn_name: tr.btn_subscribe,
+                                r#type: "email",
+                                onsubmit: |email| {
+                                    btracing::info!("Subscribed by {}", email);
+                                },
+                            }
                         }
+
+                        Socials { class: "flex flex-row gap-50" }
                     }
 
-                    Socials { class: "flex flex-row gap-50" }
-                }
-
-                div { class: "flex flex-row gap-10 items-center text-copyright font-normal text-sm/22",
-                    span { {tr.copyright} }
-                    a { class: "hover:text-white", href: "/privacy", {tr.privacy} }
-                    a { class: "hover:text-white", href: "/terms", {tr.terms} }
-                    a { class: "hover:text-white", href: "/sitemap", {tr.sitemap} }
+                    div { class: "flex flex-row gap-10 items-center text-copyright font-normal text-sm/22",
+                        span { {tr.copyright} }
+                        a { class: "hover:text-white", href: "/privacy", {tr.privacy} }
+                        a { class: "hover:text-white", href: "/terms", {tr.terms} }
+                        a { class: "hover:text-white", href: "/sitemap", {tr.sitemap} }
+                    }
                 }
             }
         }
