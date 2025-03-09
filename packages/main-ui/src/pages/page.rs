ec1d0cb3de1aad9c1e655cfe55aafee4c4d51c72
hackartists <hackartists@gmail.com>
2025-03-09 21:29:40 +0900

    feat(main-ui): add logo image and MetaPage component
    Added a new logo image to the public assets and integrated the MetaPage component in the HomePage. The MetaPage component sets the page title, description, and image.

--- packages/main-ui/src/pages/page.rs
+++ packages/main-ui/src/pages/page.rs
@@ -1,4 +1,5 @@
 #![allow(non_snake_case)]
+use by_components::meta::MetaPage;
 use dioxus::prelude::*;
 
 use super::components::*;
@@ -7,7 +8,11 @@ use dioxus_translate::*;
 
 #[component]
 pub fn HomePage(lang: Language) -> Element {
+    let tr: TopTranslate = translate(&lang);
+    let image = asset!("/public/logos/logo.png");
+
     rsx! {
+        MetaPage { title: "Ratel", description: tr.description, image: "{image}" }
         div { class: "flex flex-col w-full justify-start items-center",
             Top { lang }
             About { lang }
