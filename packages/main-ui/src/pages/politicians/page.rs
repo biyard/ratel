e3361e4db821b0e603f2dfdf04ab3546ef0c85b4
hackartists <hackartists@gmail.com>
2025-03-14 13:16:50 +0900

    feat(dropdown): add new dropdown component with Alpine.js integration
    - Added new ~Dropdown~ component with Alpine.js for better interactivity.
    - Updated ~CryptoStance~ enum with translations.
    - Introduced ~Party~ enum with translations.
    - Modified ~PoliticiansPage~ to include new dropdowns for ~CryptoStance~ and ~Party~ selection.

--- packages/main-ui/src/pages/politicians/page.rs
+++ packages/main-ui/src/pages/politicians/page.rs
@@ -1,9 +1,12 @@
 #![allow(non_snake_case)]
+use crate::components::dropdown::Dropdown;
 use crate::pages::components::SectionHeader;
 
 use super::controller::*;
 use super::i18n::*;
 use bdk::prelude::*;
+use dto::CryptoStance;
+use dto::Party;
 
 #[component]
 pub fn PoliticiansPage(lang: Language) -> Element {
@@ -14,12 +17,27 @@ pub fn PoliticiansPage(lang: Language) -> Element {
     rsx! {
         by_components::meta::MetaPage { title: tr.title, description: tr.description }
 
-
-        div { class: "flex flex-col gap-50 w-full",
+        div { class: "w-full max-w-1177 flex flex-col gap-50 w-full",
             SectionHeader {
                 section_name: tr.title,
                 title: tr.mission,
                 description: tr.description,
+            }
+
+            div { class: "flex flex-row gap-10",
+                Dropdown {
+                    items: CryptoStance::variants(&lang),
+                    onselect: move |value| {
+                        tracing::debug!("selected: {}", value);
+                    },
+                }
+
+                Dropdown {
+                    items: Party::variants(&lang),
+                    onselect: move |value| {
+                        tracing::debug!("selected: {}", value);
+                    },
+                }
             }
 
             for p in politicians.items.iter() {
