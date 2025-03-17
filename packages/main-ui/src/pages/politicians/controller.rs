97b2e7ea689b82d610bea46375d0f1cddbea979a
hackartists <hackartists@gmail.com>
2025-03-17 18:26:44 +0900

    feat(dto): add AssemblyMemberSorter enum and implement sorting
    + Added ~AssemblyMemberSorter~ enum to support sorting by name, stance, and party.
    + Updated ~AssemblyMember~ struct to include ~queryable~ attribute for sorting.
    + Enhanced ~Controller~ to handle sorting and stance signals

--- packages/main-ui/src/pages/politicians/controller.rs
+++ packages/main-ui/src/pages/politicians/controller.rs
@@ -5,24 +5,45 @@ use dto::*;
 #[derive(Debug, Clone, Copy, DioxusController)]
 pub struct Controller {
     pub politicians: Resource<QueryResponse<AssemblyMemberSummary>>,
+    pub sort: Signal<Option<AssemblyMemberSorter>>,
+    pub stance: Signal<Option<CryptoStance>>,
+    #[allow(dead_code)]
+    pub page: Signal<usize>,
 }
 
 impl Controller {
     pub fn new(_lang: Language) -> std::result::Result<Self, RenderError> {
         let size = 20;
         let page = use_signal(|| 1);
+        let sort = use_signal(|| None);
+        let stance = use_signal(|| None);
 
         let politicians = use_server_future(move || {
             let cli = AssemblyMember::get_client(&crate::config::get().main_api_endpoint);
             let page = page();
+            let sort = sort();
+            let stance = stance();
+
             async move {
-                cli.query(AssemblyMemberQuery::new(size).with_page(page))
-                    .await
-                    .unwrap_or_default()
+                let mut q = AssemblyMemberQuery::new(size).with_page(page);
+                if let Some(sort) = sort {
+                    q = q.with_sort(sort);
+                }
+
+                if let Some(stance) = stance {
+                    q = q.with_stance(stance);
+                }
+
+                cli.query(q).await.unwrap_or_default()
             }
         })?;
 
-        let ctrl = Self { politicians };
+        let ctrl = Self {
+            politicians,
+            sort,
+            page,
+            stance,
+        };
         use_context_provider(|| ctrl);
 
         Ok(ctrl)
