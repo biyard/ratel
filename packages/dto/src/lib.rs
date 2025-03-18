1892b3f38c2b20661e08981f9c2255dbdaefff87
hackartists <hackartists@gmail.com>
2025-03-19 00:08:38 +0900

    feat(dto): add votes module and integrate with bills
    - Added votes module to DTO package
    - Integrated votes with Bill struct
    - Updated main-api to remove unused modules and references
    - Enhanced politician controller to include default bill summary if no bills are present

--- packages/dto/src/lib.rs
+++ packages/dto/src/lib.rs
@@ -1,17 +1,7 @@
-mod assets;
-mod comment;
 mod error;
-mod patron;
 mod tables;
-mod topic;
-mod vote;
 
-pub use assets::*;
-pub use comment::*;
 pub use error::*;
-pub use patron::*;
 pub use tables::*;
-pub use topic::*;
-pub use vote::*;
 
 pub type Result<T> = std::result::Result<T, error::ServiceError>;
--- packages/main-ui/src/pages/politicians/_id/controller.rs
+++ packages/main-ui/src/pages/politicians/_id/controller.rs
@@ -1,5 +1,5 @@
 use bdk::prelude::*;
-use dto::AssemblyMember;
+use dto::{AssemblyMember, BillSummary};
 
 #[derive(Clone, Copy, DioxusController)]
 pub struct Controller {
@@ -16,10 +16,35 @@ impl Controller {
             async move {
                 let endpoint = crate::config::get().main_api_endpoint;
 
-                AssemblyMember::get_client(endpoint)
+                let mut res = AssemblyMember::get_client(endpoint)
                     .get(id)
                     .await
-                    .unwrap_or_default()
+                    .unwrap_or_default();
+
+                if res.bills.is_empty() {
+                    res.bills = vec![BillSummary {
+                        id: 1,
+                        created_at: 100000000,
+                        title: "DAO Treasury Transparency Act & Crypto Investor Protection Act"
+                            .to_string(),
+                        bill_no: "1".to_string(),
+                        en_title: Some(
+                            "DAO Treasury Transparency Act & Crypto Investor Protection Act"
+                                .to_string(),
+                        ),
+                        book_id: "1".to_string(),
+                        site_url: "https://example.com".to_string(),
+                        summary: Some(
+                            "This bill aims to provide transparency to DAO treasuries and protect crypto investors.".to_string(),
+                        ),
+                        en_summary: Some(
+                            "This bill aims to provide transparency to DAO treasuries and protect crypto investors.".to_string(),
+                        ),
+                        votes: vec![],
+                    }];
+                }
+
+                res
             }
         })?;
 
