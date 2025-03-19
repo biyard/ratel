85b82cdef7c2a74514d8e77febf553fe748967fe
2miwon <zhengsfsf@gmail.com>
2025-03-19 11:04:29 +0900

    fix error

--- packages/main-api/src/models/openapi/national_proposer.rs
+++ packages/main-api/src/models/openapi/national_proposer.rs
@@ -1,5 +1,6 @@
 #[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
 pub struct AssemblyProposer {
+    // https://open.assembly.go.kr/portal/data/service/selectAPIServicePage.do/OK7XM1000938DS17215
     #[serde(rename(deserialize = "BILL_NO"))]
     pub bill_no: String,
     #[serde(rename(deserialize = "PUBL_PROPOSER"))]
--- packages/main-api/src/models/openapi/national_vote.rs
+++ packages/main-api/src/models/openapi/national_vote.rs
@@ -1,5 +1,6 @@
 #[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
 pub struct AssemblyVote {
+    // https://open.assembly.go.kr/portal/data/service/selectAPIServicePage.do/OPR1MQ000998LC12535
     #[serde(rename(deserialize = "BILL_NM"))]
     pub bill_no: String,
     #[serde(rename(deserialize = "MONA_CD"))]
