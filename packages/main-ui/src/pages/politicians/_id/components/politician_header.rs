55aaf03693c187375a57e8576adc71949f060fb7
hackartists <hackartists@gmail.com>
2025-03-31 12:20:34 +0900

    feat(ui): update footer and politician header components
    + Refactor footer component for better responsiveness and styling
    + Enhance politician header component with improved layout and additional information
    + Add size parameter to CryptoStanceIcon component for flexibility

--- packages/main-ui/src/pages/politicians/_id/components/politician_header.rs
+++ packages/main-ui/src/pages/politicians/_id/components/politician_header.rs
@@ -22,54 +22,74 @@ pub fn PoliticianHeader(
     children: Element,
 ) -> Element {
     rsx! {
-        div { class: "w-full flex flex-row gap-24",
+        div { class: "w-full flex flex-row gap-24 max-mobile:!flex-col max-mobile:!gap-10 max-mobile:!items-center max-mobile:!justify-center",
             img { class: "w-233 h-260 rounded-[10px] object-cover", src: image }
 
             div { class: "grow flex flex-col justify-between",
-
-                div { id: "politician-info", class: "flex flex-col gap-24",
+                div {
+                    id: "politician-info",
+                    class: "grow flex flex-col gap-24 max-mobile:!grow",
                     h1 { class: "text-[32px]/40 font-bold text-text-primary", {name} }
 
-                    div {
-                        id: "politician-badges",
-                        class: "flex flex-col gap-13 text-text-primary font-medium text-[15px]",
+                    div { class: "grow flex flex-col justify-between max-tablet:gap-13",
+                        div {
+                            id: "politician-badges",
+                            class: "flex flex-col gap-13 text-text-primary font-medium text-[15px]",
 
-                        div { class: "flex flex-row gap-8",
-                            PartyIcon { party }
-                            {party.translate(&lang)}
-                        }
-
-                        div { class: "flex flex-row gap-8",
-                            Email {
-                                class: "[&>path]:stroke-white [&>rect]:stroke-white",
-                                width: "18",
-                                height: "18",
+                            div { class: "flex flex-row gap-8",
+                                PartyIcon { party }
+                                {party.translate(&lang)}
                             }
-                            span { {email} }
+
+                            div { class: "flex flex-row gap-8",
+                                Email {
+                                    class: "[&>path]:stroke-white [&>rect]:stroke-white",
+                                    width: "18",
+                                    height: "18",
+                                }
+                                span { {email} }
+                            }
+                        } // politician-badges
+
+                        div {
+                            id: "politician-header-crypto-stance",
+                            class: "w-full rounded-[20px] bg-bg py-24 px-24 flex flex-col gap-5 text-lg/22 font-bold text-text-primary max-tablet:!p-0 max-tablet:!bg-transparent max-tablet:text-[15px]",
+                            div { class: "flex flex-row gap-10 items-center",
+                                div { class: "max-tablet:hidden",
+                                    CryptoStanceIcon { stance }
+                                }
+                                div { class: "hidden max-tablet:!block",
+                                    CryptoStanceIcon { size: 18, stance }
+                                }
+
+                                {stance.translate(&lang)}
+                            }
+
                         }
-                    } // politician-badges
+                    }
+
                 } // politician-info
 
-                div {
-                    id: "politician-header-crypto-stance",
-                    class: "w-full rounded-[20px] bg-bg py-24 px-24 flex flex-col gap-5 text-lg/22 font-bold text-text-primary",
-                    div { class: "flex flex-row gap-10 items-center",
-                        CryptoStanceIcon { stance }
-                        {stance.translate(&lang)}
-                    }
-                }
             }
         }
     }
 }
 
 #[component]
-pub fn CryptoStanceIcon(stance: CryptoStance) -> Element {
+pub fn CryptoStanceIcon(#[props(default = 24)] size: i32, stance: CryptoStance) -> Element {
     rsx! {
-        if stance == CryptoStance::Supportive {
-            ThumbsUp { class: "[&>path]:stroke-c-c-20", width: "24", height: "24" }
-        } else if stance == CryptoStance::Against {
-            ThumbsDown { class: "[&>path]:stroke-c-p-20", width: "24", height: "24" }
+        if stance == CryptoStance::Supportive || stance == CryptoStance::StronglySupportive {
+            ThumbsUp {
+                class: "[&>path]:stroke-c-c-20",
+                width: "{size}",
+                height: "{size}",
+            }
+        } else if stance == CryptoStance::Against || stance == CryptoStance::StronglyAgainst {
+            ThumbsDown {
+                class: "[&>path]:stroke-c-p-20",
+                width: "{size}",
+                height: "{size}",
+            }
         }
     }
 }
