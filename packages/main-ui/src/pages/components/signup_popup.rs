7475d5806c6df9923e2522bc75df6f832c107ae5
hackartists <hackartists@gmail.com>
2025-04-03 06:43:55 +0900

    refactor(signup_popup): comment out wallet signup section
    Commented out the wallet signup section in the SignupPopup component for future revisions.

--- packages/main-ui/src/pages/components/signup_popup.rs
+++ packages/main-ui/src/pages/components/signup_popup.rs
@@ -47,19 +47,19 @@ pub fn SignupPopup(
                         }
                     }
 
-                    div {
-                        class: "w-full flex flex-row pl-20 py-22 bg-black border-[1px] rounded-[10px] justify-start items-center gap-17 cursor-pointer border-black hover:border-white",
-                        onclick: move |_| {
-                            tracing::debug!("signup with wallet clicked");
-                            popup.open(rsx! {
-                                WalletPopup { lang }
-                            });
-                        },
-                        icons::Wallet {}
-                        div { class: "flex flex-col gap-3",
-                            span { class: "text-white text-base/19 font-semibold", "{tr.connect_wallet}" }
-                        }
-                    }
+                // div {
+                //     class: "w-full flex flex-row pl-20 py-22 bg-black border-[1px] rounded-[10px] justify-start items-center gap-17 cursor-pointer border-black hover:border-white",
+                //     onclick: move |_| {
+                //         tracing::debug!("signup with wallet clicked");
+                //         popup.open(rsx! {
+                //             WalletPopup { lang }
+                //         });
+                //     },
+                //     icons::Wallet {}
+                //     div { class: "flex flex-col gap-3",
+                //         span { class: "text-white text-base/19 font-semibold", "{tr.connect_wallet}" }
+                //     }
+                // }
                 }
                 SigninPopupFooter { lang }
             }
