cabe1ecfeb765d527a4a4bc9caf91f7ebde0cca5
hackartists <hackartists@gmail.com>
2025-04-02 17:02:25 +0900

    feat: add mobile Chrome configuration and improve mobile menu handling in tests
    Added a new configuration for mobile Chrome using Pixel 5. Enhanced the home page test to handle mobile menu interactions

--- playwright.config.ts
+++ playwright.config.ts
@@ -57,6 +57,11 @@ export default defineConfig({
       },
     },
 
+    {
+      name: "mobile-chrome",
+      use: { ...devices["Pixel 5"] },
+    },
+
     // {
     //   name: "webkit",
     //   use: { ...devices["Desktop Safari"] },
--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -5,13 +5,40 @@ test("[Home-001] Looking over website w/o signing up", async ({
   page,
 }, testInfo) => {
   const p = wrap(page, testInfo.project.name, "home/001-look-over");
+
   await p.goto("/", { waitUntil: "load" });
   await p.fullCapture("full");
   await p.capture("top");
 
+  const viewport = page.viewportSize();
+
+  if (viewport && viewport.width < 768) {
+    await p.clickXpathAndCapture(
+      "/html/body/div/div[3]/div[2]/div[1]/div/div[1]/button[1]",
+      "Mobile menu",
+    );
+  }
   await p.clickXpathAndCapture("//nav/a[1]", "About");
+  if (viewport && viewport.width < 768) {
+    await p.clickXpathAndCapture(
+      "/html/body/div/div[3]/div[2]/div[1]/div/div[1]/button[1]",
+      "Mobile menu",
+    );
+  }
   await p.clickXpathAndCapture("//nav/a[2]", "Politician stance");
+  if (viewport && viewport.width < 768) {
+    await p.clickXpathAndCapture(
+      "/html/body/div/div[3]/div[2]/div[1]/div/div[1]/button[1]",
+      "Mobile menu",
+    );
+  }
   await p.clickXpathAndCapture("//nav/a[3]", "Community");
+  if (viewport && viewport.width < 768) {
+    await p.clickXpathAndCapture(
+      "/html/body/div/div[3]/div[2]/div[1]/div/div[1]/button[1]",
+      "Mobile menu",
+    );
+  }
   await p.clickXpathAndCapture("//nav/a[4]", "Support");
 
   const to_politicians = p.getByRole("link", { name: "View all" });
