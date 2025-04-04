bcfb7ceb12681698265e84a962d4d414a6220617
hackartists <hackartists@gmail.com>
2025-04-04 13:01:00 +0900

    fix scroll position after refresh

--- packages/main-ui/src/pages/page.rs
+++ packages/main-ui/src/pages/page.rs
@@ -10,6 +10,32 @@ pub fn HomePage(lang: Language) -> Element {
     let tr: TopTranslate = translate(&lang);
     let image = asset!("/public/logos/logo.png");
 
+    use_effect(|| {
+        use wasm_bindgen::JsCast;
+        use web_sys::{Element, window};
+
+        if let Some(window) = window() {
+            let location = window.location();
+            if let Ok(hash) = location.hash() {
+                tracing::debug!("hash :{hash}");
+
+                if !hash.is_empty() {
+                    let document = window.document().unwrap();
+                    // web_sys::console::log_1(&hash);
+                    if let Some(target) = document.query_selector(&hash).ok().flatten() {
+                        let scroll = web_sys::ScrollIntoViewOptions::new();
+                        scroll.set_behavior(web_sys::ScrollBehavior::Smooth);
+
+                        let _ = target
+                            .dyn_ref::<Element>()
+                            .unwrap()
+                            .scroll_into_view_with_scroll_into_view_options(&scroll);
+                    }
+                }
+            }
+        }
+    });
+
     rsx! {
         MetaPage { title: "Ratel", description: tr.description, image: "{image}" }
         div { class: "w-full flex flex-col justify-start items-center",
