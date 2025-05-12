01e02f83a3243b58b8eac6048beca7a6c13936db
2miwon <zhengsfsf@gmail.com>
2025-05-13 04:12:54 +0900

    chore: delete comment

--- packages/dto/src/tables/us_bills/us_bill.rs
+++ packages/dto/src/tables/us_bills/us_bill.rs
@@ -9,13 +9,3 @@ pub struct USBill {
     #[api_model(summary, auto = [insert, update])]
     pub updated_at: i64,
 }
-
-impl USBill {
-    // pub fn detail_date(&self, str_date: &str) -> String {
-    //     let date = chrono::NaiveDate::parse_from_str(str_date, "%Y-%m-%d").unwrap_or_default();
-    //     let start = date.format("%b %d").to_string();
-    //     let end = date.format(" - %b %d, %Y").to_string();
-
-    //     format!("{}{}", start, end)
-    // }
-}
