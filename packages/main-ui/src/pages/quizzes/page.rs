09c877c5be76177fcb20e74602b8b9ce97b9c969
hackartists <hackartists@gmail.com>
2025-05-18 14:49:34 +0900

    feat(main-ui): add quizzes feature with pages, components, and controllers
    - Introduced a new quizzes feature, including pages, components, and controllers.
    - Updated `Cargo.toml` to include `dioxus-motion` dependency and feature configuration.
    - Enhanced `custom.css` with new styles for buttons and headings.
    - Modified `ThumbsUp` and `ThumbsDown` components to support additional attributes and dynamic sizing.
    - Added `QuizzesPage` and `ResultsPage` with corresponding controllers and i18n support.
    - Updated routing to include `/quizzes` and `/quizzes/results/:id` paths.

---
+++ packages/main-ui/src/pages/quizzes/page.rs
@@ -0,0 +1,66 @@
+use crate::components::icons::{ThumbsDown, ThumbsUp};
+
+use super::*;
+use bdk::prelude::*;
+use controller::*;
+use i18n::*;
+
+#[component]
+pub fn QuizzesPage(#[props(default = Language::En)] lang: Language) -> Element {
+    let mut ctrl = Controller::new(lang)?;
+    let tr: QuizzesTranslate = translate(&lang);
+
+    rsx! {
+        by_components::meta::MetaPage { title: tr.title }
+
+        div {
+            id: "quizzes",
+            class: "absolute transition-all duration-500 left-0 top-0 h-screen w-screen flex flex-col items-center py-50 px-20 justify-between aria-started:left-[-100%]",
+            "aria-started": ctrl.started(),
+            h1 { class: "heading1 whitespace-pre-line text-center py-30", {tr.title} }
+            dotlottie-player {
+                src: asset!("/public/animations/ani_logo.json"),
+                class: "w-193 h-250",
+                "autoplay": true,
+            }
+
+            button { class: "btn primary w-full", onclick: move |_| ctrl.start(), {tr.btn_start} }
+        } // end of this page
+
+        for (i , quiz) in ctrl.quizzes()?.into_iter().enumerate() {
+            div {
+                id: "quiz-{i}",
+                class: "absolute transition-all duration-500 left-[{ctrl.left(i)}%] top-0 h-screen w-screen flex flex-col items-center py-50 px-20 justify-between",
+
+                div { class: "grow-1 h-full flex flex-col items-center justify-center gap-10",
+                    h2 { class: "heading1 !text-primary font-black", "Q{i+1}." }
+                    QuizItem { quiz: quiz.clone() }
+                }
+                div { class: "w-full flex flex-row justify-around",
+                    button {
+                        class: "group btn !flex-col items-center",
+                        onclick: move |_| async move {
+                            ctrl.like(i).await;
+                        },
+                        ThumbsUp {
+                            class: "group-hover:[&>path]:fill-primary group-hover:[&>path]:stroke-primary",
+                            size: 50,
+                        }
+                        {tr.btn_like}
+                    }
+                    button {
+                        class: "group btn !flex-col items-center",
+                        onclick: move |_| async move {
+                            ctrl.dislike(i).await;
+                        },
+                        ThumbsDown {
+                            class: "group-hover:[&>path]:fill-primary group-hover:[&>path]:stroke-primary",
+                            size: 50,
+                        }
+                        {tr.btn_dislike}
+                    }
+                }
+            }
+        }
+    }
+}
