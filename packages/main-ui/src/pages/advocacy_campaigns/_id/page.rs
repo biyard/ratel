ed1b38171bf56dae316bd1563cba44093ad06f43
hackartists <hackartists@gmail.com>
2025-05-27 15:49:02 +0900

    feat(ui): improve responsiveness and styling for advocacy campaign pages
    - Added responsive styles for `h2` and `p` elements in `main.css` to enhance mobile experience.
    - Updated layout and styling in `page.rs` for better responsiveness on smaller screens:
      - Adjusted grid layout to flex for mobile devices.
      - Modified button and author section styles for consistency.
      - Enhanced vote count display with highlighted text for better visibility.

--- packages/main-ui/src/pages/advocacy_campaigns/_id/page.rs
+++ packages/main-ui/src/pages/advocacy_campaigns/_id/page.rs
@@ -24,9 +24,9 @@ pub fn AdvocacyCampaignsByIdPage(
     rsx! {
         by_components::meta::MetaPage { title: campaign.title.clone() }
 
-        div { class: "absolute bottom-30 flex items-center justify-center w-full z-100",
+        div { class: "absolute fixed bg-background py-30 bottom-0 flex items-center justify-center w-full z-100 px-20",
             button {
-                class: "group max-w-desktop w-full btn primary flex items-center justify-center aria-voted:!bg-secondary/30 aria-voted:!text-secondary",
+                class: "group max-w-desktop w-full btn primary flex items-center justify-center aria-voted:!bg-neutral-600 aria-voted:!text-secondary",
                 "aria-voted": campaign.voted,
                 onclick: move |_| async move { ctrl.handle_agree().await },
                 span { class: "group-aria-voted:!hidden", {tr.btn_agree} }
@@ -36,12 +36,12 @@ pub fn AdvocacyCampaignsByIdPage(
 
         div {
             id: "advocacy-campaings-by-id",
-            class: "relative max-w-desktop min-h-screen flex flex-col gap-50 px-20 pb-100 pt-80 justify-start items-center",
-            h1 { class: "heading1", {campaign.title} }
+            class: "max-w-desktop min-h-screen flex flex-col gap-50 px-20 pb-100 pt-80 justify-start items-center max-tablet:!gap-10",
+            h1 { class: "heading1 max-tablet:!text-3xl", {campaign.title} }
 
             div {
                 id: "advocacy-campaign-content",
-                class: "grid grid-cols-3 gap-10 w-full items-start",
+                class: "grid grid-cols-3 gap-10 w-full items-start max-tablet:!flex max-tablet:!flex-col",
 
                 article {
                     id: "advocacy-campaign-details",
@@ -49,25 +49,29 @@ pub fn AdvocacyCampaignsByIdPage(
                     dangerous_inner_html: campaign.contents,
                 }
 
-                div { class: "col-span-1 order-2 max-tablet:order-1 flex flex-col items-start gap-20",
-                    div { class: "w-full bg-component-bg p-20 flex flex-col gap-30 items-start rounded-lg",
-                        h2 { class: "text-xl font-bold", {tr.author} }
+                div { class: "col-span-1 order-2 max-tablet:order-1 flex flex-col items-start gap-20 max-tablet:!gap-5",
+                    div { class: "w-full bg-component-bg p-20 flex flex-col gap-30 items-start rounded-lg max-tablet:!bg-transparent max-tablet:!gap-10 max-tablet:!p-0",
+                        h2 { class: "text-xl font-bold max-tablet:!hidden", {tr.author} }
                         div { class: "flex flex-col gap-10",
                             div { class: "w-full flex flex-row gap-10 items-center",
                                 img {
-                                    class: "w-50 object-cover rounded-lg",
+                                    class: "w-50 object-cover rounded-lg max-tablet:!w-20",
                                     src: author.profile_url.clone(),
                                 }
-                                p { class: "font-bold", {author.nickname.clone()} }
+                                p { class: "font-bold max-tablet:!text-c-wg-50",
+                                    {author.nickname.clone()}
+                                }
                             }
                             p {
-                                class: "flex flex-col text-c-wg-70",
+                                class: "flex flex-col text-c-wg-70 max-tablet:!hidden",
                                 dangerous_inner_html: author.html_contents.clone(),
                             }
                         }
                     }
-                    p { class: " bg-component-bg rounded-lg p-20 w-full",
-                        "지금까지 {campaign.votes}명이 이 법안을 지지하였습니다."
+                    p { class: " bg-component-bg rounded-lg p-20 w-full max-tablet:!bg-transparent max-tablet:!p-0",
+                        "지금까지 "
+                        span { class: "text-primary font-bold", "{campaign.votes}명" }
+                        "이 이 법안을 지지하였습니다."
                     }
                 }
             }
