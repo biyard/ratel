605fc06b3b316dc6bf68ed90b925cade9c6481d3
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2025-05-29 09:04:03 +0900

    relocate directory location

--- packages/main-ui/src/pages/threads/_id/spaces/_id/mod.rs
+++ packages/main-ui/src/pages/threads/_id/spaces/_id/mod.rs
@@ -1,10 +1,16 @@
 mod components;
 mod controller;
+mod deliberation;
 mod i18n;
+mod legislation;
+mod nft;
 mod page;
-mod pages;
+mod poll;
 
 pub use components::*;
+pub use deliberation::*;
 pub use i18n::*;
+pub use legislation::*;
+pub use nft::*;
 pub use page::*;
-pub use pages::*;
+pub use poll::*;
