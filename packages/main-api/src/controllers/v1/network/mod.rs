1e9747bf63b905921ea44a8d67f5f757e5e5c976
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2025-06-18 11:38:53 +0900

    implement network controller

--- packages/main-api/src/controllers/v1/network/mod.rs
+++ packages/main-api/src/controllers/v1/network/mod.rs
@@ -1,4 +1,5 @@
 use crate::by_axum::axum::routing::get;
+use crate::utils::users::extract_user_id;
 use bdk::prelude::*;
 use by_axum::{
     auth::Authorization,
@@ -15,12 +16,57 @@ pub struct NetworkController {
 }
 
 impl NetworkController {
-    async fn find_one(&self, _auth: Option<Authorization>) -> Result<NetworkData> {
-        let _pool = self.pool.clone();
+    async fn find_one(&self, auth: Option<Authorization>) -> Result<NetworkData> {
+        let industries = Industry::query_builder()
+            .query()
+            .map(Industry::from)
+            .fetch_all(&self.pool)
+            .await?;
+
+        let current_user_id = extract_user_id(&self.pool, auth.clone()).await?;
+
+        let suggested_teams_sql = r#"
+            SELECT *
+            FROM users u
+            WHERE u.id != $1
+            AND u.user_type = $2
+            AND u.id NOT IN (
+                SELECT following_id FROM my_networks WHERE follower_id = $1
+            )
+            ORDER BY RANDOM()
+            LIMIT 3
+        "#;
+
+        let suggested_users_sql = r#"
+            SELECT *
+            FROM users u
+            WHERE u.id != $1
+            AND u.user_type = $2
+            AND u.id NOT IN (
+                SELECT following_id FROM my_networks WHERE follower_id = $1
+            )
+            ORDER BY RANDOM()
+            LIMIT 5
+        "#;
+
+        let suggested_teams = sqlx::query(suggested_teams_sql)
+            .bind(current_user_id)
+            .bind(UserType::Team as i32)
+            .map(Follower::from)
+            .fetch_all(&self.pool)
+            .await?;
+
+        let suggested_users = sqlx::query(suggested_users_sql)
+            .bind(current_user_id)
+            .bind(UserType::Individual as i32)
+            .map(Follower::from)
+            .fetch_all(&self.pool)
+            .await?;
+
         Ok(NetworkData {
-            industries: vec![],
-            suggested_teams: vec![],
-            suggested_users: vec![],
+            industries,
+            suggested_teams,
+            suggested_users,
         })
     }
 }
