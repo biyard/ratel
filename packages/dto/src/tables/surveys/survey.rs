3ec3b3b172f6166afda7eec9a0d15a0429452730
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2025-06-23 11:51:48 +0900

    implement space survey

---
+++ packages/dto/src/tables/surveys/survey.rs
@@ -0,0 +1,71 @@
+use bdk::prelude::*;
+use validator::Validate;
+
+#[derive(Validate)]
+#[api_model(base = "/v1/spaces/:space-id/surveys", table = surveys)]
+pub struct Survey {
+    #[api_model(summary, primary_key)]
+    pub id: i64,
+    #[api_model(summary, auto = [insert])]
+    pub created_at: i64,
+    #[api_model(summary, auto = [insert, update])]
+    pub updated_at: i64,
+
+    #[api_model(many_to_one = spaces)]
+    pub space_id: i64,
+    #[api_model(summary, type = INTEGER, action_by_id = update)]
+    #[serde(default)]
+    pub status: ProjectStatus,
+
+    #[api_model(summary, action = create, action_by_id = update)]
+    #[serde(default)]
+    pub started_at: i64,
+    #[api_model(summary, action = create, action_by_id = update)]
+    #[serde(default)]
+    pub ended_at: i64,
+
+    #[api_model(summary, action = create, type = JSONB, version = v0.1, action_by_id = update)]
+    #[serde(default)]
+    pub questions: Vec<Question>,
+
+    #[api_model(summary, one_to_many = survey_responses, foreign_key = survey_id, aggregator = count)]
+    #[serde(default)]
+    pub response_count: i64,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, serde::Serialize, serde::Deserialize)]
+#[cfg_attr(feature = "server", derive(schemars::JsonSchema, aide::OperationIo))]
+#[serde(rename_all = "snake_case", tag = "answer_type")]
+pub enum Question {
+    SingleChoice(ChoiceQuestion),
+    MultipleChoice(ChoiceQuestion),
+    ShortAnswer(SubjectiveQuestion),
+    Subjective(SubjectiveQuestion),
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, serde::Serialize, serde::Deserialize, Default)]
+#[cfg_attr(feature = "server", derive(schemars::JsonSchema, aide::OperationIo))]
+pub struct SubjectiveQuestion {
+    pub title: String,
+    pub description: String,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, serde::Serialize, serde::Deserialize, Default)]
+#[cfg_attr(feature = "server", derive(schemars::JsonSchema, aide::OperationIo))]
+pub struct ChoiceQuestion {
+    pub title: String,
+    pub description: Option<String>,
+    pub options: Vec<String>,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Default, ApiModel, Translate, Copy)]
+#[cfg_attr(feature = "server", derive(schemars::JsonSchema, aide::OperationIo))]
+pub enum ProjectStatus {
+    #[default]
+    #[translate(ko = "준비")]
+    Ready = 1,
+    #[translate(ko = "진행")]
+    InProgress = 2,
+    #[translate(ko = "마감")]
+    Finish = 3,
+}
