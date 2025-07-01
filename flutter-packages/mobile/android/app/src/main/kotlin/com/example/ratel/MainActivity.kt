a83377f2ae23f7ecce5a7b29bf488c99ffebeb7a
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2025-07-01 10:52:20 +0900

    reinitialize flutter mobile

--- flutter-packages/mobile/android/app/src/main/kotlin/com/example/ratel/MainActivity.kt
+++ flutter-packages/mobile/android/app/src/main/kotlin/com/example/ratel/MainActivity.kt
@@ -1,5 +1,5 @@
-package com.example.ratel
+package foundation.ratel.app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
