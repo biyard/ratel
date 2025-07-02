bcbcf5c9fcd8e95ee99b75deb1580073461e78cf
ryancho-0 <ryan@biyard.co>
2025-07-02 12:54:14 +0900

    fix: lint error

--- ts-packages/web/src/app/layout.tsx
+++ ts-packages/web/src/app/layout.tsx
@@ -12,8 +12,6 @@ import { prefetchUserInfo } from './(social)/_hooks/user';
 import Script from 'next/script';
 import { getServerQueryClient } from '@/lib/query-utils.server';
 
-// TODO: REMOVE THIS COMMENT
- 
 const raleway = Raleway({
   variable: '--font-raleway',
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
