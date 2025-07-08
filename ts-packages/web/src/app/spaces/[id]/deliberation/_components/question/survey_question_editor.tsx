d789ec423a791121c035409371f31da0143acb00
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2025-07-08 13:48:32 +0900

    fix survey ui

--- ts-packages/web/src/app/spaces/[id]/deliberation/_components/question/survey_question_editor.tsx
+++ ts-packages/web/src/app/spaces/[id]/deliberation/_components/question/survey_question_editor.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import AnswerTypeSelect, { AnswerType } from './answer_type_select';
 import { Input } from '@/components/ui/input';
 import { Trash2 } from 'lucide-react';
-import { EditOption } from '@/components/icons';
+import { DialPad, DialPad2, Remove } from '@/components/icons';
 
 export default function SurveyQuestionEditor({
   index,
@@ -90,56 +90,65 @@ export default function SurveyQuestionEditor({
   };
 
   return (
-    <div className="flex flex-col w-full border border-neutral-700 rounded-md p-4 gap-3">
-      <div className="flex gap-2 max-tablet:flex-col">
-        <AnswerTypeSelect value={questionType} onChange={handleTypeChange} />
-        <Input
-          type="text"
-          placeholder="Input Question Title."
-          value={questionTitle}
-          onChange={(e) => handleTitleChange(e.target.value)}
-        />
+    <div className="flex flex-col w-full bg-[#191919] rounded-[10px] px-[16px] pb-[20px] pt-[4px]">
+      <div className="flex flex-row w-full justify-center items-center mb-[10px]">
+        <DialPad className="w-6 h-6" />
       </div>
-
-      {(questionType === 'single_choice' ||
-        questionType === 'multiple_choice') && (
-        <div className="flex flex-col gap-2">
-          {questionOptions.map((opt, idx) => (
-            <div
-              key={`option-${index}-${idx}`}
-              className="flex gap-2 items-center"
-            >
-              <div className="w-4 h-4 rounded-full border border-neutral-400" />
-              <Input
-                type="text"
-                placeholder={`Option ${idx + 1}`}
-                value={opt}
-                onChange={(e) => handleOptionChange(idx, e.target.value)}
-              />
-              <EditOption
-                className="cursor-pointer w-4 h-4 stroke-neutral-400 text-neutral-400"
-                onClick={() => handleRemoveOption(idx)}
-              />
-            </div>
-          ))}
-          <button
-            onClick={addOption}
-            className="cursor-pointer text-sm text-neutral-500 font-semibold mt-1 text-left"
-          >
-            + Add Option
-          </button>
+      <div className="flex flex-col w-full gap-2.5">
+        <div className="flex gap-2 max-tablet:flex-col">
+          <AnswerTypeSelect value={questionType} onChange={handleTypeChange} />
+          <Input
+            className="bg-neutral-800 border border-neutral-700 rounded-lg w-full px-[16px] !py-[22px] font-medium text-[15px]/[22.5px] text-white placeholder:text-neutral-600 "
+            type="text"
+            placeholder="Title"
+            value={questionTitle}
+            onChange={(e) => handleTitleChange(e.target.value)}
+          />
         </div>
-      )}
 
-      <div className="flex flex-row w-full justify-end items-center">
-        <div
-          className="cursor-pointer flex flex-row w-fit gap-[5px]"
-          onClick={() => onremove?.(index)}
-        >
-          <div className="text-sm text-neutral-500 font-medium cursor-pointer">
-            Delete
+        <div className="flex flex-col mt-[10px] gap-[10px]">
+          {(questionType === 'single_choice' ||
+            questionType === 'multiple_choice') && (
+            <div className="flex flex-col gap-2">
+              {questionOptions.map((opt, idx) => (
+                <div
+                  key={`option-${index}-${idx}`}
+                  className="flex gap-[10px] items-center"
+                >
+                  <DialPad2 className="w-6 h-6" />
+                  <Input
+                    className="border-b border-transparent !border-b-white focus:!border-transparent focus:rounded-md font-normal text-base/[24px] placeholder:text-neutral-600 text-neutral-300 rounded-none"
+                    type="text"
+                    placeholder={`Type something...`}
+                    value={opt}
+                    onChange={(e) => handleOptionChange(idx, e.target.value)}
+                  />
+                  <Remove
+                    className="cursor-pointer w-5 h-5 stroke-neutral-400 text-neutral-400"
+                    onClick={() => handleRemoveOption(idx)}
+                  />
+                </div>
+              ))}
+              <button
+                onClick={addOption}
+                className="cursor-pointer text-sm text-neutral-500 font-semibold text-left mt-2"
+              >
+                + Add Option
+              </button>
+            </div>
+          )}
+        </div>
+
+        <div className="flex flex-row w-full justify-end items-center">
+          <div
+            className="cursor-pointer flex flex-row w-fit gap-[5px] items-center"
+            onClick={() => onremove?.(index)}
+          >
+            <div className="text-[15px] text-neutral-500 font-medium cursor-pointer">
+              Delete
+            </div>
+            <Trash2 className="w-[18px] h-[18px] stroke-neutral-500 cursor-pointer" />
           </div>
-          <Trash2 className="w-[18px] h-[18px] stroke-neutral-500 cursor-pointer" />
         </div>
       </div>
     </div>
