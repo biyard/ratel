c6a776e8b591b95dbb5b75dc344d16c922439682
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2025-07-10 11:35:06 +0900

    fix by comment

--- ts-packages/web/src/app/spaces/[id]/deliberation/_components/modal/new_discussion.tsx
+++ ts-packages/web/src/app/spaces/[id]/deliberation/_components/modal/new_discussion.tsx
@@ -24,13 +24,6 @@ export default function NewDiscussion({
 
   const [startTime, setStartTime] = useState<number>(discussion.started_at);
   const [endTime, setEndTime] = useState<number>(discussion.ended_at);
-  const [startTimeDropdownOpen, setStartTimeDropdownOpen] =
-    useState<boolean>(false);
-  const [endTimeDropdownOpen, setEndTimeDropdownOpen] =
-    useState<boolean>(false);
-  const [startCalendarOpen, setStartCalendarOpen] = useState<boolean>(false);
-  const [endCalendarOpen, setEndCalendarOpen] = useState<boolean>(false);
-
   return (
     <div className="max-w-[900px] w-full">
       <div className="flex flex-col py-2.5 gap-[5px]">
@@ -72,55 +65,31 @@ export default function NewDiscussion({
         <div className="flex flex-row gap-2.5 items-center">
           <CalendarDropdown
             value={startTime}
-            calendarOpen={startCalendarOpen}
-            setCalendarOpen={(value: boolean) => {
-              setStartCalendarOpen(value);
-              setStartTimeDropdownOpen(false);
-            }}
             onChange={(date) => {
               const newStart = Math.floor(date);
               setStartTime(newStart);
-              setStartCalendarOpen(false);
             }}
           />
           <TimeDropdown
             value={startTime}
-            timeDropdownOpen={startTimeDropdownOpen}
-            setTimeDropdownOpen={(value: boolean) => {
-              setStartTimeDropdownOpen(value);
-              setStartCalendarOpen(false);
-            }}
             onChange={(timestamp) => {
               const newStart = Math.floor(timestamp);
               setStartTime(newStart);
-              setStartTimeDropdownOpen(false);
             }}
           />
           <div className="w-[15px] h-0.25 bg-neutral-600" />
           <CalendarDropdown
             value={endTime}
-            calendarOpen={endCalendarOpen}
-            setCalendarOpen={(value: boolean) => {
-              setEndCalendarOpen(value);
-              setEndTimeDropdownOpen(false);
-            }}
             onChange={(date) => {
               const newEnd = Math.floor(date);
               setEndTime(newEnd);
-              setEndCalendarOpen(false);
             }}
           />
           <TimeDropdown
             value={endTime}
-            timeDropdownOpen={endTimeDropdownOpen}
-            setTimeDropdownOpen={(value: boolean) => {
-              setEndTimeDropdownOpen(value);
-              setEndCalendarOpen(false);
-            }}
             onChange={(timestamp) => {
               const newEnd = Math.floor(timestamp);
               setEndTime(newEnd);
-              setEndTimeDropdownOpen(false);
             }}
           />
           <div className="flex flex-row items-center w-fit border border-c-wg-70 rounded-lg px-5 py-[10.5px] gap-2.5">
