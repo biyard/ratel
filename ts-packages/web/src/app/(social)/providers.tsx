c9dbeadcfc1f0a006901f52d7b62bf4a5dd58c28
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2025-07-24 15:41:12 +0900

    fix my posts, drafts loading

--- ts-packages/web/src/app/(social)/providers.tsx
+++ ts-packages/web/src/app/(social)/providers.tsx
@@ -3,6 +3,7 @@ import { initData, InitDataOptions } from '@/providers/getQueryClient';
 import {
   getFeedById,
   getNetwork,
+  getPostByUserId,
   getPosts,
   getPromotion,
   getUserInfo,
@@ -12,16 +13,30 @@ import { getServerQueryClient } from '@/lib/query-utils.server';
 import { dehydrate } from '@tanstack/react-query';
 import { ratelApi } from '@/lib/api/ratel_api';
 import { apolloServerClient } from '@/lib/apollo';
+import { FeedStatus } from '@/lib/api/models/feeds';
 
 export default async function Provider({ children }: { children: ReactNode }) {
   const queryClient = await getServerQueryClient();
+  const network = await getNetwork();
   const promotion = await getPromotion();
+  const user = await getUserInfo();
+  const post = await getPosts(1, 10);
+
+  const myPosts = await getPostByUserId(user.data?.id ?? 0, 1, 20);
+  const myDraftPosts = await getPostByUserId(
+    user.data?.id ?? 0,
+    1,
+    20,
+    FeedStatus.Draft,
+  );
 
   const data: InitDataOptions[] = [
-    await getNetwork(),
+    network,
     promotion,
-    await getUserInfo(),
-    await getPosts(1, 10),
+    user,
+    post,
+    myPosts,
+    myDraftPosts,
   ];
 
   if (promotion.data) {
