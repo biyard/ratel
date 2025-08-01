d40c217e77bd2084b246992278bfc8dba01f0a26
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2025-08-01 20:35:22 +0900

    add export icon

---
+++ ts-packages/web/src/assets/icons/shapes/index.tsx
@@ -0,0 +1,7 @@
+export { default as Ellipse } from './ellipse.svg';
+export { default as Hexagon } from './hexagon.svg';
+export { default as Pentagon } from './pentagon.svg';
+export { default as Pentagon2 } from './pentagon-2.svg';
+export { default as Square } from './square.svg';
+export { default as Star } from './star.svg';
+export { default as Triangle } from './triangle.svg';
