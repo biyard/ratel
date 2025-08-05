8612e647c9b4ea2ace001921ae9171b49f33997e
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2025-08-05 16:01:23 +0900

    fix metadata logic

--- packages/main-api/src/controllers/v1/assets.rs
+++ packages/main-api/src/controllers/v1/assets.rs
@@ -44,8 +44,72 @@ impl AssetController {
     pub fn route(&self) -> Result<by_axum::axum::Router> {
         Ok(Router::new()
             .route("/", get(Self::get_put_object_uri))
-            .route("/complete", post(Self::complete_multipart_upload))
+            .route("/multipart", get(Self::get_put_multi_object_uri))
+            .route("/multipart/complete", post(Self::complete_multipart_upload))
             .with_state(self.clone()))
+    }
+
+    pub async fn get_put_object_uri(
+        State(ctrl): State<AssetController>,
+        Query(req): Query<AssetPresignedUrisReadAction>,
+    ) -> Result<Json<AssetPresignedUris>> {
+        use aws_sdk_s3::presigning::PresigningConfig;
+        use uuid::Uuid;
+
+        use aws_config::BehaviorVersion;
+        use aws_config::{Region, defaults};
+        use aws_sdk_s3::config::Credentials;
+
+        let config = defaults(BehaviorVersion::latest())
+            .region(Region::new(ctrl.config.region))
+            .credentials_provider(Credentials::new(
+                ctrl.config.access_key_id,
+                ctrl.config.secret_access_key,
+                None,
+                None,
+                "ratel",
+            ));
+
+        let config = config.load().await;
+
+        let client = aws_sdk_s3::Client::new(&config);
+
+        tracing::debug!("/aws/s3/put-uri: {:?}", req);
+        let mut presigned_uris = vec![];
+        let mut uris = vec![];
+        let total_count = req.total_count.unwrap_or(1);
+        for _ in 0..total_count {
+            let id = Uuid::new_v4();
+            let key = format!("{}/{}", ctrl.asset_dir, id);
+
+            let presigned_request = client
+                .put_object()
+                .bucket(ctrl.bucket_name)
+                .key(key.clone())
+                .presigned(
+                    PresigningConfig::expires_in(std::time::Duration::from_secs(ctrl.expire))
+                        .map_err(|e| {
+                            tracing::error!("Failed to set expired time {}", e.to_string());
+                            Error::AssetError(e.to_string())
+                        })?,
+                )
+                .await
+                .map_err(|e| {
+                    tracing::error!("Failed to put object {}", e.to_string());
+                    Error::AssetError(e.to_string())
+                })?;
+            presigned_uris.push(presigned_request.uri().to_string());
+            uris.push(format!("https://{}/{}", ctrl.bucket_name, key));
+        }
+
+        Ok(Json(AssetPresignedUris {
+            presigned_uris,
+            uris,
+            total_count,
+            file_type: FileType::None,
+            upload_id: None,
+            key: None,
+        }))
     }
 
     pub async fn complete_multipart_upload(
@@ -108,7 +172,7 @@ impl AssetController {
         Ok(Json(final_url))
     }
 
-    pub async fn get_put_object_uri(
+    pub async fn get_put_multi_object_uri(
         State(ctrl): State<AssetController>,
         Query(req): Query<AssetPresignedUrisReadAction>,
     ) -> Result<Json<AssetPresignedUris>> {
