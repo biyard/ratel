78328bc5228615917020c307dcb2ecb6535b5619
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2025-08-11 15:31:24 +0900

    implement select topic

--- flutter-packages/mobile/lib/presentations/select_topic/select_topic_controller.dart
+++ flutter-packages/mobile/lib/presentations/select_topic/select_topic_controller.dart
@@ -1,3 +1,36 @@
 import 'package:ratel/exports.dart';
 
-class SelectTopicController extends BaseController {}
+class SelectTopicController extends BaseController {
+  //FIXME: fix to query api
+  final industries = [
+    "Crypto",
+    'AI',
+    'Mobility',
+    'Bio & Healthcare',
+    'Semiconductor',
+    'Defense Tech',
+    'Smart City',
+    'Content & Entertainment',
+    'Fintech',
+    'Green Tech',
+    'Security',
+  ];
+
+  final query = ''.obs;
+  final selected = <String>{}.obs;
+
+  List<String> get filtered => industries
+      .where((e) => e.toLowerCase().contains(query.value.toLowerCase()))
+      .toList();
+
+  void onSearchChanged(String v) => query.value = v;
+  void toggle(String tag) =>
+      selected.contains(tag) ? selected.remove(tag) : selected.add(tag);
+
+  void goBack() => Get.rootDelegate.offNamed(AppRoutes.setupProfileScreen);
+  void skip() => Get.rootDelegate.offNamed(AppRoutes.welcomeScreen);
+  void next() => {
+    logger.d("selected: $selected"),
+    // Get.rootDelegate.offNamed(AppRoutes.welcomeScreen)
+  };
+}
