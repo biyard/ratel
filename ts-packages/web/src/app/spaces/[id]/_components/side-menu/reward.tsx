deb549ca657778ee9dae3ed53d378324717aa252
ryancho-0 <ryan@biyard.co>
2025-08-11 12:42:57 +0900

    feat(web): impl space-side-menu

---
+++ ts-packages/web/src/app/spaces/[id]/_components/side-menu/reward.tsx
@@ -0,0 +1,84 @@
+import BlackBox from '@/app/(social)/_components/black-box';
+import { RewardCoin } from '@/assets/icons/money-payment';
+import { Settings2 } from '@/assets/icons/settings';
+import { ReactNode } from 'react';
+
+interface RewardProps {
+  amount: number;
+  text: string;
+}
+
+interface ModifierProps {
+  icon: ReactNode;
+  multiple: number;
+  text: string;
+  color: string;
+}
+export default function RewardMenu({
+  isEditing,
+  handleSetting,
+  rewardItems,
+  modifierItems,
+}: {
+  isEditing: boolean;
+  handleSetting: () => void;
+  rewardItems: RewardProps[];
+  modifierItems: ModifierProps[];
+}) {
+  return (
+    <BlackBox>
+      <div className="flex flex-col w-full text-neutral-500 gap-5">
+        <div className="flex flex-row font-bold text-sm justify-between">
+          <div className="flex flex-row gap-2">
+            <RewardCoin className="size-5 [&>*]:stroke-neutral-500" />
+            Rewards
+          </div>
+          {isEditing && (
+            <Settings2
+              className="size-5 [&>*]:stroke-neutral-500 cursor-pointer"
+              onClick={handleSetting}
+            />
+          )}
+        </div>
+
+        {rewardItems.map((item) => (
+          <RewardItem key={item.text} amount={item.amount} text={item.text} />
+        ))}
+        <div className="flex flex-col gap-2 text-sm">
+          {modifierItems.map((item) => (
+            <ModifierItem key={item.text} {...item} />
+          ))}
+        </div>
+      </div>
+    </BlackBox>
+  );
+}
+
+function RewardItem({ amount, text }: RewardProps) {
+  return (
+    <div className="flex flex-row w-full gap-2.5 text-[12px]/[12px] text-neutral-500">
+      <span>+</span>
+      <div className="flex flex-col gap-1">
+        <span className="text-white text-[15px]/[12px] font-medium">
+          {amount.toLocaleString()} P
+        </span>
+        <span>{text}</span>
+      </div>
+    </div>
+  );
+}
+function ModifierItem({ icon, multiple, text, color }: ModifierProps) {
+  return (
+    <div
+      className={`w-full flex flex-row
+        bg-${color}/5 text-${color} text-[15px]/[12px] font-bold
+        justify-between items-center px-2.5 py-2.5 rounded-sm `}
+    >
+      <div className="[&>svg]:size-6 flex flex-row items-center justify-center gap-2">
+        {icon}
+        <span>X {multiple}</span>
+      </div>
+      <span>{text}</span>
+    </div>
+  );
+}
