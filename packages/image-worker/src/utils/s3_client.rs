2e1cc00e867f19d8bf97ee3795cea73aaac6158f
ryancho-0 <ryan@biyard.co>
2025-08-20 10:58:27 +0900

    feat(image-worker): add image processing worker with S3 integration and watermarking functionality

---
+++ packages/image-worker/src/utils/s3_client.rs
@@ -0,0 +1,147 @@
+// FIXME: This code is copy of `packages/main-api/src/utils/s3_upload.rs`
+// Please remove this file and use the one from `packages/common` instead.
+
+use std::sync::Arc;
+
+use dto::{Error, Result};
+
+use aws_config::{BehaviorVersion, Region, defaults};
+use aws_sdk_s3::config::Credentials;
+use aws_sdk_s3::presigning::PresigningConfig;
+
+use crate::config::{self, BucketConfig};
+pub struct PresignedUrl {
+    pub presigned_uris: Vec<String>,
+    pub uris: Vec<String>,
+    pub total_count: i32,
+}
+
+pub struct S3Client {
+    pub client: aws_sdk_s3::Client,
+}
+
+impl S3Client {
+    pub async fn new() -> Result<Arc<Self>> {
+        let conf = crate::config::get();
+        let aws_conf = defaults(BehaviorVersion::latest())
+            .region(Region::new(conf.aws.region))
+            .credentials_provider(Credentials::new(
+                conf.aws.access_key_id,
+                conf.aws.secret_access_key,
+                None,
+                None,
+                "ratel",
+            ));
+
+        let sdk_config = aws_conf.load().await;
+
+        let client = aws_sdk_s3::Client::new(&sdk_config);
+
+        Ok(Arc::new(Self { client }))
+    }
+
+    pub async fn get_put_object_uri(&self, total_count: i32) -> Result<PresignedUrl> {
+        use uuid::Uuid;
+
+        let mut presigned_uris = vec![];
+        let mut uris = vec![];
+        let BucketConfig {
+            name,
+            asset_dir,
+            expire,
+        } = config::get().bucket;
+
+        for _ in 0..total_count {
+            let id = Uuid::new_v4();
+            let key = format!("{}/{}", asset_dir, id);
+
+            let presigned_request =
+                self.client
+                    .put_object()
+                    .bucket(name)
+                    .key(key.clone())
+                    .presigned(
+                        PresigningConfig::expires_in(std::time::Duration::from_secs(expire))
+                            .map_err(|e| {
+                                tracing::error!("Failed to set expired time {}", e.to_string());
+                                Error::AssetError(e.to_string())
+                            })?,
+                    )
+                    .await
+                    .map_err(|e| {
+                        tracing::error!("Failed to put object {}", e.to_string());
+                        Error::AssetError(e.to_string())
+                    })?;
+            presigned_uris.push(presigned_request.uri().to_string());
+            uris.push(format!("https://{}/{}", name, key));
+        }
+
+        Ok(PresignedUrl {
+            presigned_uris,
+            uris,
+            total_count,
+        })
+    }
+}
+
+// pub async fn get_put_object_uri(
+//     aws_config: &AwsConfig,
+//     aws_bucket_config: &BucketConfig,
+//     total_count: Option<i32>,
+// ) -> Result<PresignedUrl> {
+//     use aws_sdk_s3::presigning::PresigningConfig;
+//     use uuid::Uuid;
+
+//     use aws_config::BehaviorVersion;
+//     use aws_config::{Region, defaults};
+//     use aws_sdk_s3::config::Credentials;
+//     let total_count = total_count.unwrap_or(1);
+//     let config = defaults(BehaviorVersion::latest())
+//         .region(Region::new(aws_config.region))
+//         .credentials_provider(Credentials::new(
+//             aws_config.access_key_id,
+//             aws_config.secret_access_key,
+//             None,
+//             None,
+//             "ratel",
+//         ));
+
+//     let config = config.load().await;
+
+//     let client = aws_sdk_s3::Client::new(&config);
+
+//     let mut presigned_uris = vec![];
+//     let mut uris = vec![];
+
+//     for _ in 0..total_count {
+//         let id = Uuid::new_v4();
+//         let key = format!("{}/{}", aws_bucket_config.asset_dir, id);
+
+//         let presigned_request = client
+//             .put_object()
+//             .bucket(aws_bucket_config.name)
+//             .key(key.clone())
+//             .presigned(
+//                 PresigningConfig::expires_in(std::time::Duration::from_secs(
+//                     aws_bucket_config.expire,
+//                 ))
+//                 .map_err(|e| {
+//                     tracing::error!("Failed to set expired time {}", e.to_string());
+//                     Error::AssetError(e.to_string())
+//                 })?,
+//             )
+//             .await
+//             .map_err(|e| {
+//                 tracing::error!("Failed to put object {}", e.to_string());
+//                 Error::AssetError(e.to_string())
+//             })?;
+//         presigned_uris.push(presigned_request.uri().to_string());
+//         uris.push(format!("https://{}/{}", aws_bucket_config.name, key));
+//     }
+
+//     Ok(PresignedUrl {
+//         presigned_uris,
+//         uris,
+//         total_count,
+//     })
+// }
---
+++ packages/image-worker/src/utils/watermark.rs
@@ -0,0 +1,88 @@
+use std::sync::Arc;
+
+use base64::{Engine, engine::general_purpose};
+use dto::{Artwork, ArtworkRepositoryUpdateRequest, Error, File, Result, reqwest, sqlx::PgPool};
+
+use crate::utils::s3_client::{PresignedUrl, S3Client};
+
+pub async fn process_watermark_async(
+    pool: &PgPool,
+    s3_client: Arc<S3Client>,
+    artwork_id: i64,
+    original_url: String,
+) -> Result<()> {
+    let bytes = read_image_from_url(&original_url).await?;
+    let watermarked_bytes = visible_watermarking(bytes)?;
+
+    let PresignedUrl {
+        presigned_uris,
+        uris,
+        total_count: _,
+    } = s3_client.get_put_object_uri(1).await?;
+
+    let client = reqwest::Client::new();
+    client
+        .put(presigned_uris[0].clone())
+        .body(watermarked_bytes)
+        .send()
+        .await?;
+
+    Artwork::get_repository(pool.clone())
+        .update(
+            artwork_id,
+            ArtworkRepositoryUpdateRequest {
+                file: Some(File {
+                    url: Some(uris[0].clone()),
+                    ..Default::default()
+                }),
+                ..Default::default()
+            },
+        )
+        .await?;
+
+    Ok(())
+}
+
+async fn read_image_from_url(url: &str) -> Result<Vec<u8>> {
+    let client = reqwest::Client::builder()
+        .timeout(std::time::Duration::from_secs(30))
+        .build()?;
+    let bytes = client.get(url).send().await?.bytes().await?;
+    Ok(bytes.to_vec())
+}
+
+fn visible_watermarking(slice: Vec<u8>) -> Result<Vec<u8>> {
+    let mut orig = photon_rs::PhotonImage::new_from_byteslice(slice);
+
+    let width = orig.get_width();
+    let height = orig.get_height();
+    if width == 0 || height == 0 {
+        tracing::error!("Image has zero width or height");
+        return Err(Error::BadRequest);
+    }
+    // Update the path below to the correct location of your watermark image file
+    let logo = include_bytes!("./protected.png");
+
+    let logo = general_purpose::STANDARD.encode(logo.as_ref());
+
+    let wm = photon_rs::PhotonImage::new_from_base64(&logo);
+
+    let wm_width = wm.get_width();
+    let wm_height = wm.get_height();
+
+    let w_width = (width as f32 * 0.8) as u32;
+    let w_height = ((w_width as f32 / wm_width as f32) * wm_height as f32) as u32;
+    let wm = photon_rs::transform::resize(
+        &wm,
+        w_width,
+        w_height,
+        photon_rs::transform::SamplingFilter::Nearest,
+    );
+    let x = (width - w_width) / 2;
+    let y = (height - w_height) / 2;
+
+    photon_rs::multiple::watermark(&mut orig, &wm, x as i64, y as i64);
+    let bytes = orig.get_bytes();
+
+    Ok(bytes)
+}
