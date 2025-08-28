0300942ecde0ad22a8f45ac61c8e2ad98969ad58
ryancho-0 <ryan@biyard.co>
2025-08-28 23:58:54 +0900

    fix: bug

--- packages/main-api/src/route.rs
+++ packages/main-api/src/route.rs
@@ -4,6 +4,7 @@ use bdk::prelude::*;
 
 use crate::{
     controllers::{
+        self,
         m2::noncelab::users::register_users::{
             RegisterUserResponse, register_users_by_noncelab_handler,
         },
@@ -54,44 +55,44 @@ use axum::native_routing::get as nget;
 use axum::native_routing::post as npost;
 use axum::routing::{get_with, post_with};
 
-macro_rules! wrap_api {
-    (
-        $method:expr,
-        $handler:expr,
-        $success_ty:ty,
-        $summary:expr,
-        $description:expr
-    ) => {
-        $method($handler, |op| {
-            op.summary($summary)
-                .description($description)
-                .response_with::<200, axum::Json<$success_ty>, _>(|res| {
-                    res.description("Success response")
-                })
-                .response_with::<400, axum::Json<dto::Error>, _>(|res| {
-                    res.description("Incorrect or invalid requests")
-                        .example(dto::Error::UserAlreadyExists)
-                })
-        })
-    };
-}
+// macro_rules! wrap_api {
+//     (
+//         $method:expr,
+//         $handler:expr,
+//         $success_ty:ty,
+//         $summary:expr,
+//         $description:expr
+//     ) => {
+//         $method($handler, |op| {
+//             op.summary($summary)
+//                 .description($description)
+//                 .response_with::<200, axum::Json<$success_ty>, _>(|res| {
+//                     res.description("Success response")
+//                 })
+//                 .response_with::<400, axum::Json<dto::Error>, _>(|res| {
+//                     res.description("Incorrect or invalid requests")
+//                         .example(dto::Error::UserAlreadyExists)
+//                 })
+//         })
+//     };
+// }
 
-macro_rules! post_api {
-    (
-        $handler:expr,
-        $success_ty:ty,
-        $summary:expr,
-        $description:expr
-    ) => {
-        wrap_api!(
-            axum::routing::post_with,
-            $handler,
-            $success_ty,
-            $summary,
-            $description
-        )
-    };
-}
+// macro_rules! post_api {
+//     (
+//         $handler:expr,
+//         $success_ty:ty,
+//         $summary:expr,
+//         $description:expr
+//     ) => {
+//         wrap_api!(
+//             axum::routing::post_with,
+//             $handler,
+//             $success_ty,
+//             $summary,
+//             $description
+//         )
+//     };
+// }
 
 macro_rules! api_docs {
     ($success_ty:ty, $summary:expr, $description:expr) => {
@@ -130,9 +131,14 @@ pub async fn route(
     private_s3_client: S3Client,
 ) -> Result<by_axum::axum::Router> {
     Ok(by_axum::axum::Router::new()
-        .native_route("/users/logout", npost(logout_handler))
+        .nest("/v1", controllers::v1::route(pool.clone()).await?)
+        .nest(
+            "/m1",
+            controllers::m1::MenaceController::route(pool.clone())?,
+        )
+        .native_route("/v2/users/logout", npost(logout_handler))
         .route(
-            "/industries/select-topics",
+            "/v2/industries/select-topics",
             post_with(
                 select_topics_handler,
                 api_docs!("Select Topics", "Select interesting topics"),
@@ -140,7 +146,7 @@ pub async fn route(
             .with_state(pool.clone()),
         )
         .route(
-            "/industries",
+            "/v2/industries",
             get_with(
                 list_industries_handler,
                 api_docs!("List Industries", "List industry types"),
@@ -148,7 +154,7 @@ pub async fn route(
             .with_state(pool.clone()),
         )
         .route(
-            "/networks",
+            "/v2/networks",
             get_with(
                 list_networks_handler,
                 api_docs!(
@@ -159,7 +165,7 @@ pub async fn route(
             .with_state(pool.clone()),
         )
         .route(
-            "/networks/search",
+            "/v2/networks/search",
             get_with(
                 list_networks_by_keyword_handler,
                 api_docs!(
@@ -170,7 +176,7 @@ pub async fn route(
             .with_state(pool.clone()),
         )
         .route(
-            "/networks/follow",
+            "/v2/networks/follow",
             post_with(
                 follow_handler,
                 api_docs!("Follow Users", "Follow users with follower IDs"),
@@ -178,7 +184,7 @@ pub async fn route(
             .with_state(pool.clone()),
         )
         .route(
-            "/bookmarks/add",
+            "/v2/bookmarks/add",
             post_with(
                 add_bookmark_handler,
                 api_docs!("Add Bookmarks", "Add Feed Bookmarks with user ID"),
@@ -186,7 +192,7 @@ pub async fn route(
             .with_state(pool.clone()),
         )
         .route(
-            "/bookmarks/remove",
+            "/v2/bookmarks/remove",
             post_with(
                 remove_bookmark_handler,
                 api_docs!("Remove Bookmarks", "Remove Feed Bookmarks with user ID"),
@@ -194,7 +200,7 @@ pub async fn route(
             .with_state(pool.clone()),
         )
         .route(
-            "/bookmarks",
+            "/v2/bookmarks",
             get_with(
                 get_bookmarks_handler,
                 api_docs!("List Bookmarks", "Retrieve bookmarked feed with user ID"),
@@ -202,7 +208,7 @@ pub async fn route(
             .with_state(pool.clone()),
         )
         .route(
-            "/users",
+            "/v2/users",
             get_with(
                 find_user_handler,
                 api_docs!(
@@ -213,7 +219,7 @@ pub async fn route(
             .with_state(pool.clone()),
         )
         .route(
-            "/dashboards",
+            "/v2/dashboards",
             get_with(
                 get_dashboard_handler,
                 api_docs!("Get Dashboards", "Retrieve dashboard in a service"),
@@ -221,7 +227,7 @@ pub async fn route(
             .with_state(pool.clone()),
         )
         .route(
-            "/dagits/:space_id",
+            "/v2/dagits/:space_id",
             get_with(
                 get_dagit_handler,
                 api_docs!("Get Dagit by space ID", "Retrieve dagit in a space"),
@@ -229,7 +235,7 @@ pub async fn route(
             .with_state(pool.clone()),
         )
         .route(
-            "/dagits/:space_id/oracles",
+            "/v2/dagits/:space_id/oracles",
             post_with(
                 add_oracle_handler,
                 api_docs!("Add Oracle", "Add a new oracle to a dagit"),
@@ -237,7 +243,7 @@ pub async fn route(
             .with_state(pool.clone()),
         )
         .route(
-            "/dagits/:space_id/artworks",
+            "/v2/dagits/:space_id/artworks",
             post_with(
                 create_artwork_handler,
                 api_docs!("Create Artwork", "Create a new artwork for a dagit"),
@@ -245,7 +251,7 @@ pub async fn route(
             .with_state((pool.clone(), sqs_client.clone())),
         )
         .route(
-            "/dagits/:space_id/consensus",
+            "/v2/dagits/:space_id/consensus",
             post_with(
                 create_consensus_handler,
                 api_docs!("Start Dagit Consensus", "Start a new consensus for a dagit"),
@@ -253,7 +259,7 @@ pub async fn route(
             .with_state(pool.clone()),
         )
         .route(
-            "/artworks/:artwork_id",
+            "/v2/artworks/:artwork_id",
             get_with(
                 get_artwork_detail_handler,
                 api_docs!("Get Artwork", "Retrieve a specific artwork"),
@@ -261,7 +267,7 @@ pub async fn route(
             .with_state(pool.clone()),
         )
         .route(
-            "/my-spaces",
+            "/v2/my-spaces",
             get_with(
                 get_my_space_controller,
                 api_docs!("Get My Space", "Retrieve a spaces"),
@@ -269,7 +275,7 @@ pub async fn route(
             .with_state(pool.clone()),
         )
         .route(
-            "/dagits/:space_id/artworks/:artwork_id/vote",
+            "/v2/dagits/:space_id/artworks/:artwork_id/vote",
             post_with(
                 consensus_vote_handler,
                 api_docs!(
@@ -280,7 +286,7 @@ pub async fn route(
             .with_state(pool.clone()),
         )
         .route(
-            "/artworks/:artwork_id/certificate",
+            "/v2/artworks/:artwork_id/certificate",
             get_with(
                 get_artwork_certificate_handler,
                 api_docs!("Get Artwork", "Retrieve a specific artwork"),
@@ -297,40 +303,51 @@ pub async fn route(
         )
         .route(
             "/v2/documents",
-            get_with(upload_private_image_handler, api_docs!(
-                "Get S3 Presigned URL for Uploading Private Image",
-                "This endpoint provides presigned URLs for uploading private images to S3.\n\n**Authorization header required**\n\n"
-            ))
+            get_with(
+                upload_private_image_handler,
+                api_docs!(
+                    "Get S3 Presigned URL for Uploading Private Image",
+                    r#"This endpoint provides presigned URLs for uploading private images to S3.
+                **Authorization header required**
+                `Authorization: Bearer <token>`"#
+                ),
+            )
             .with_state(UploadPrivateImageState {
                 s3_client: private_s3_client.clone(),
-            })
+            }),
         )
         .route(
             "/v2/documents/passport",
-            post_with(extract_passport_info_handler, api_docs!(
-                "Extract Information from Passport Image",
-                "This endpoint allows you to extract passport information from an image.\n\n**Authorization header required**\n\n"
-            ))
+            post_with(
+                extract_passport_info_handler,
+                api_docs!(
+                    "Extract Information from Passport Image",
+                    r#"This endpoint allows you to extract passport information from an image.
+                
+                **Authorization header required**"#
+                ),
+            )
             .with_state(PassportHandlerState {
                 pool: pool.clone(),
                 bedrock_client: bedrock_client.clone(),
                 rek_client: rek_client.clone(),
                 textract_client: textract_client.clone(),
                 s3_client: private_s3_client.clone(),
-            })
+            }),
         )
         .route(
             "/v2/telegram/subscribe",
-            post_api!(
+            post_with(
                 telegram_subscribe_handler,
-                (),
-                "Subscribe to Telegram",
-                "This endpoint allows users to subscribe to Telegram notifications."
+                api_docs!(
+                    "Subscribe to Telegram",
+                    "This endpoint allows users to subscribe to Telegram notifications."
+                ),
             )
             .with_state(pool.clone()),
         )
         .route(
-            "/spaces/:space_id/delete",
+            "/v2/spaces/:space_id/delete",
             post_with(
                 delete_space_handler,
                 api_docs!(
@@ -342,27 +359,29 @@ pub async fn route(
             .with_state(pool.clone()),
         )
         .route(
-            "/notifications/mark-all-read",
-            post_api!(
+            "/v2/notifications/mark-all-read",
+            post_with(
                 mark_all_notifications_read_handler,
-                (),
-                "Mark All Notifications Read",
-                "Mark all notifications as read for the authenticated user."
+                api_docs!(
+                    "Mark All Notifications Read",
+                    "Mark all notifications as read for the authenticated user."
+                ),
             )
             .with_state(pool.clone()),
         )
         .route(
             "/m2/noncelab/users",
-            post_api!(
+            post_with(
                 register_users_by_noncelab_handler,
-                RegisterUserResponse,
-                "Register users by Noncelab",
-                //NOTE: This text blocking `rustfmt`
-                concat!(
-                    "This endpoint allows you to register users by Noncelab.\n\n",
-                    "**Authorization header required**\n\n",
-                    "`Authorization: Bearer <token>`"
-                )
+                api_docs!(
+                    RegisterUserResponse,
+                    "Register users by Noncelab",
+                    r#"This endpoint allows you to register users by Noncelab.
+                    
+                    **Authorization header required**
+                    
+                    `Authorization: Bearer <token>`"#
+                ),
             )
             .with_state(pool.clone()),
         )
