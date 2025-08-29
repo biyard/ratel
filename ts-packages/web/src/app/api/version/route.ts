8f70ccf06b9dbb1e5dd78996279552d9f3d22e75
hackartists <hackartists@gmail.com>
2025-08-29 12:28:51 +0900

    fix lint

--- ts-packages/web/src/app/api/version/route.ts
+++ ts-packages/web/src/app/api/version/route.ts
@@ -1,7 +1,7 @@
 import { config } from '@/config';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   return new NextResponse({
     version: config.version,
   });
