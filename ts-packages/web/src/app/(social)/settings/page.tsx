6020953d9e8c5f9593d1a7f733dad7f4acd40468
Jamesunekwuojo <unekwuojoabah4@gmail.com>
2025-08-29 02:19:44 +0100

    started working on settings tab..

--- ts-packages/web/src/app/(social)/settings/page.tsx
+++ ts-packages/web/src/app/(social)/settings/page.tsx
@@ -2,7 +2,24 @@
 
 import React from 'react';
 import MyInfo from './_components/tab/my-info';
+import MySettings from './_components/tab/my-settings';
 
 export default function MyProfilePage() {
-  return <MyInfo />;
+  return (
+    <div>
+
+      {/* tab active state indicator */}
+      <div className="w-10 h-[2px] bg-primary rounded-md"></div>
+
+      {/* Top tabs section */}
+      <div className= "flex flex-row justify-between text-sm font-bold  text-neutral-400 w-[500px] mx-auto">
+        <p>My Info</p>
+        <p>My Inventory</p>
+        <p>Settings</p>
+      </div>
+
+      {/* Tab content */}
+        
+    </div>
+  );
 }
