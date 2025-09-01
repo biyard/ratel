21bf87e5dca46e01683f7b76e2b3196a676e2e03
hackartists <hackartists@gmail.com>
2025-09-01 12:14:40 +0900

    Add Next.js dockerfile

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -8,12 +8,13 @@ let stackName = `${process.env.PROJECT}-${process.env.SERVICE}-${process.env.ENV
 if (process.env.STACK) {
   stackName = process.env.STACK;
 }
-stackName = "ratel-api2-dev-stack"
+const env = process.env.ENV || "dev";
+stackName = "ratel-dev-stack"
 // Common host
-const host = "api2.dev.ratel.foundation";
+const host = "api.dev.ratel.foundation";
 
 // --- Regional stacks (ALB + Fargate) ---
-const eu = new RegionalServiceStack(app, "Svc-EU", {
+const eu = new RegionalServiceStack(app, `ratel-${env}-svc-eu-central-1`, {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: "eu-central-1" },
@@ -22,7 +23,7 @@ const eu = new RegionalServiceStack(app, "Svc-EU", {
   commit: process.env.COMMIT!,
 });
 
-const us = new RegionalServiceStack(app, "Svc-US", {
+const us = new RegionalServiceStack(app, `ratel-${env}-svc-us-east-1`, {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: "us-east-1" },
@@ -31,7 +32,7 @@ const us = new RegionalServiceStack(app, "Svc-US", {
   commit: process.env.COMMIT!,
 });
 
-const kr = new RegionalServiceStack(app, "Svc-KR", {
+const kr = new RegionalServiceStack(app, `ratel-${env}-svc-ap-northeast-2`, {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: "ap-northeast-2" },
@@ -43,7 +44,7 @@ const kr = new RegionalServiceStack(app, "Svc-KR", {
 // --- Global Accelerator + Route53 stack ---
 // crossRegionReferences=true in all stacks lets us pass ALBs between regions
 new GlobalAccelStack(app, "GlobalAccel", {
-  stackName,
+  stackName: `ratel-${env}-stack`,
   // GA is a global service; pick any region for the stack (us-west-2 is common)
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
   fullDomainName: host,
