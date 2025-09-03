c4fe485fadca3cf54ad8e87ad9a42eb7413d5b57
Jamesunekwuojo <unekwuojoabah4@gmail.com>
2025-09-03 05:16:04 +0100

    refactored pattern for token definition

--- ts-packages/web/src/app/theme-wrapper.tsx
+++ ts-packages/web/src/app/theme-wrapper.tsx
@@ -1,9 +1,20 @@
 'use client';
 
+import { ThemeProvider } from 'next-themes';
+
 export default function ThemeWrapper({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div className="bg-background">{children}</div>;
+  return (
+    <ThemeProvider
+      attribute="data-theme"
+      defaultTheme="system"
+      enableSystem
+      storageKey="theme"
+    >
+      {children}
+    </ThemeProvider>
+  );
 }
