ef14a4d056b46532d847e8f5672555178098bdbc
blikq <blikqmain@gmail.com>
2025-09-10 18:24:46 +0100

    components fix

--- ts-packages/web/src/app/(social)/_components/News.tsx
+++ ts-packages/web/src/app/(social)/_components/News.tsx
@@ -1,33 +1,56 @@
+'use client';
+import { LoadingIndicator } from '@/app/loading';
 import { Col } from '@/components/ui/col';
-import { NewsSummary } from '@/lib/api/models/home';
+import { ratelApi } from '@/lib/api/ratel_api';
+import { useSuspenseQuery } from '@apollo/client';
 import { useTranslations } from 'next-intl';
+import {
+  ErrorBoundary,
+  ErrorComponent,
+} from 'next/dist/client/components/error-boundary';
 import { useRouter } from 'next/navigation';
-import React from 'react';
+import React, { Suspense } from 'react';
 
-interface NewsProps {
-  newsData: NewsSummary[];
+export interface NewsItem {
+  id: number;
+  title: string;
+  html_content: string;
+  created_at: number;
 }
 
-export default function News({ newsData }: NewsProps) {
+const Error: ErrorComponent = ({ error }) => {
+  console.error('Error occurred:', error);
+  return <div></div>;
+};
+export default function Wrapper() {
+  return (
+    <ErrorBoundary errorComponent={Error}>
+      <Suspense fallback={<LoadingIndicator />}>
+        <News />
+      </Suspense>
+    </ErrorBoundary>
+  );
+}
+function News() {
   const t = useTranslations('Home');
   const router = useRouter();
+  const q = ratelApi.graphql.listNews(3);
+  const {
+    data: { news },
+  }: { data: { news: NewsItem[] } } = useSuspenseQuery(q.query, {
+    variables: q.variables,
+  });
 
   const handleNewsNavigation = (id: number) => {
     router.push(`/news/${id}`);
   };
-
-  // Don't render if no news data
-  if (!newsData || newsData.length === 0) {
-    return null;
-  }
-
   return (
     <Col className="w-full rounded-[10px] bg-card px-4 py-5 mt-[10px]">
       <h3 className="text-[15px]/[20px] tracking-[0.5px] font-bold text-foreground">
         {t('latest_news')}
       </h3>
       <Col className="gap-3.75">
-        {newsData.map((item) => (
+        {news.map((item) => (
           <Col
             onClick={() => handleNewsNavigation(item.id)}
             key={`news-${item.id}`}
