1c489293474121eaa2b91884ecd80baba9d5ebd5
hackartists <hackartists@gmail.com>
2025-09-16 09:47:14 +0900

    Add post models

---
+++ packages/main-api/src/models/dynamo_tables/main/feeds/post_summary.rs
@@ -0,0 +1,10 @@
+use super::*;
+use bdk::prelude::*;
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, DynamoEntity)]
+#[serde(untagged)]
+pub enum PostSummary {
+    Post(Post),
+    PostAuthor(PostAuthor),
+    PostSpace(PostSpace),
+}
--- packages/main-api/src/models/dynamo_tables/main/mod.rs
+++ packages/main-api/src/models/dynamo_tables/main/mod.rs
@@ -1,2 +1,3 @@
 pub mod email_verification;
+pub mod feeds;
 pub mod user;
