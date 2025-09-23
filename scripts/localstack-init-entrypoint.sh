23e680bcb8d166ca0196401510c6248079f0dad6
ryancho-0 <ryan@biyard.co>
2025-09-23 20:37:40 +0900

    fix(workflow): add LocalStack health check in PR workflow and update AWS endpoint URL

--- scripts/localstack-init-entrypoint.sh
+++ scripts/localstack-init-entrypoint.sh
@@ -5,28 +5,20 @@ yum update
 yum install -y awscli
 
 echo 'Waiting for LocalStack to be ready...'
-export AWS_ENDPOINT_URL="${LOCALSTACK_ENDPOINT:-http://localhost:4566}"
-echo "Using AWS endpoint URL: $AWS_ENDPOINT_URL"
-wait_for_localstack() {
-  echo "Waiting for LocalStack to be ready..."
-  until curl -s "${AWS_ENDPOINT_URL}/_localstack/health" | jq -e '.services.dynamodb == "running" and .services.sqs == "running"' >/dev/null 2>&1; do
+until aws dynamodb --endpoint-url=http://localstack:4566  list-tables >/dev/null 2>&1; do
     sleep 2
-  done
-  echo "LocalStack is ready."
-}
-wait_for_localstack
-
+done
 echo 'Creating ratel-local table with GSIs...'
-aws --endpoint-url=${LOCALSTACK_ENDPOINT} dynamodb create-table --cli-input-json file:///scripts/dynamodb-schema.json
+aws --endpoint-url=http://localstack:4566 dynamodb create-table --cli-input-json file:///scripts/dynamodb-schema.json
 echo 'ratel-local-main table and GSIs created successfully'
-aws --endpoint-url=${LOCALSTACK_ENDPOINT} dynamodb create-table --cli-input-json file:///scripts/dynamodb-session-schema.json
+aws --endpoint-url=http://localstack:4566 dynamodb create-table --cli-input-json file:///scripts/dynamodb-session-schema.json
 echo 'ratel-local-session table and GSIs created successfully'
 
 echo 'Waiting for LocalStack to be ready...'
-until aws --endpoint-url=${LOCALSTACK_ENDPOINT} sqs list-queues >/dev/null 2>&1; do
+until aws --endpoint-url=http://localstack:4566 sqs list-queues >/dev/null 2>&1; do
     sleep 2
 done &&
 echo 'Creating SQS queues...'
-aws --endpoint-url=${LOCALSTACK_ENDPOINT} sqs create-queue --queue-name watermark-queue
-aws --endpoint-url=${LOCALSTACK_ENDPOINT} sqs create-queue --queue-name artwork-image-queue
+aws --endpoint-url=http://localstack:4566 sqs create-queue --queue-name watermark-queue 
+aws --endpoint-url=http://localstack:4566 sqs create-queue --queue-name artwork-image-queue
 echo 'SQS queues created successfully'
