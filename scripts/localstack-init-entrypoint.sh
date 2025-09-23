77ef3fe4247dd836caa373b4597975cd0bb3d1da
ryancho-0 <ryan@biyard.co>
2025-09-23 20:17:51 +0900

    fix(workflow): use LOCALSTACK_ENDPOINT variable for AWS CLI commands in scripts

--- scripts/localstack-init-entrypoint.sh
+++ scripts/localstack-init-entrypoint.sh
@@ -5,20 +5,20 @@ yum update
 yum install -y awscli
 
 echo 'Waiting for LocalStack to be ready...'
-until aws dynamodb --endpoint-url=http://localstack:4566  list-tables >/dev/null 2>&1; do
+until aws dynamodb --endpoint-url=${LOCALSTACK_ENDPOINT}  list-tables >/dev/null 2>&1; do
     sleep 2
 done
 echo 'Creating ratel-local table with GSIs...'
-aws --endpoint-url=http://localstack:4566 dynamodb create-table --cli-input-json file:///scripts/dynamodb-schema.json
+aws --endpoint-url=${LOCALSTACK_ENDPOINT} dynamodb create-table --cli-input-json file:///scripts/dynamodb-schema.json
 echo 'ratel-local-main table and GSIs created successfully'
-aws --endpoint-url=http://localstack:4566 dynamodb create-table --cli-input-json file:///scripts/dynamodb-session-schema.json
+aws --endpoint-url=${LOCALSTACK_ENDPOINT} dynamodb create-table --cli-input-json file:///scripts/dynamodb-session-schema.json
 echo 'ratel-local-session table and GSIs created successfully'
 
 echo 'Waiting for LocalStack to be ready...'
-until aws --endpoint-url=http://localstack:4566 sqs list-queues >/dev/null 2>&1; do
+until aws --endpoint-url=${LOCALSTACK_ENDPOINT} sqs list-queues >/dev/null 2>&1; do
     sleep 2
 done &&
 echo 'Creating SQS queues...'
-aws --endpoint-url=http://localstack:4566 sqs create-queue --queue-name watermark-queue 
-aws --endpoint-url=http://localstack:4566 sqs create-queue --queue-name artwork-image-queue
+aws --endpoint-url=${LOCALSTACK_ENDPOINT} sqs create-queue --queue-name watermark-queue
+aws --endpoint-url=${LOCALSTACK_ENDPOINT} sqs create-queue --queue-name artwork-image-queue
 echo 'SQS queues created successfully'
