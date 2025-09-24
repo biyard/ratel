4908af4a7dc2c828357c629fda8c3ad2697e612b
ryancho-0 <ryan@biyard.co>
2025-09-24 17:25:22 +0900

    fix(main-api): fix find user handler

--- packages/main-api/src/controllers/v3/users/find_user.rs
+++ packages/main-api/src/controllers/v3/users/find_user.rs
@@ -12,27 +12,29 @@ use dto::{
 };
 use serde::Deserialize;
 
-use validator::Validate;
-
-#[derive(Debug, Clone, Deserialize, aide::OperationIo, JsonSchema, Validate)]
+#[derive(Debug, Clone, Deserialize, aide::OperationIo, JsonSchema)]
 pub struct FindUserQueryParams {
-    #[schemars(description = "email address")]
-    #[validate(email)]
-    pub email: Option<String>,
-    #[schemars(description = "username")]
-    pub username: Option<String>,
-    #[schemars(description = "User's phone number")]
-    pub phone_number: Option<String>,
+    #[schemars(description = "query type")]
+    pub r#type: FindUserQueryType,
+    #[schemars(description = "query value")]
+    pub value: String,
 }
 
+#[derive(Debug, Clone, Deserialize, JsonSchema)]
+pub enum FindUserQueryType {
+    Email,
+    Username,
+    PhoneNumber,
+}
 pub type FindUserResponse = UserDetailResponse;
 
 pub async fn find_user_handler(
     State(AppState { dynamo, .. }): State<AppState>,
     Query(params): Query<FindUserQueryParams>,
 ) -> Result<Json<FindUserResponse>, Error2> {
-    let user_pk = match (params.email, params.username, params.phone_number) {
-        (Some(email), None, None) => {
+    let user_pk = match params.r#type {
+        FindUserQueryType::Email => {
+            let email = params.value;
             let (users, _bookmark) =
                 User::find_by_email(&dynamo.client, &email, Default::default()).await?;
             if users.len() == 0 {
@@ -43,7 +45,8 @@ pub async fn find_user_handler(
             }
             users.into_iter().nth(0).unwrap().pk
         }
-        (None, Some(username), None) => {
+        FindUserQueryType::Username => {
+            let username = params.value;
             let (users, _bookmark) =
                 User::find_by_username(&dynamo.client, &username, Default::default()).await?;
             if users.len() == 0 {
@@ -54,7 +57,8 @@ pub async fn find_user_handler(
             }
             users.into_iter().nth(0).unwrap().pk
         }
-        (None, None, Some(phone_number)) => {
+        FindUserQueryType::PhoneNumber => {
+            let phone_number = params.value;
             let (users, _bookmark) = UserPhoneNumber::find_by_phone_number(
                 &dynamo.client,
                 &phone_number,
@@ -69,12 +73,8 @@ pub async fn find_user_handler(
             }
             users.into_iter().nth(0).unwrap().pk
         }
-        _ => {
-            return Err(Error2::BadRequest(
-                "One and only one of email, username, or phoneNumber must be provided".to_string(),
-            ));
-        }
     };
+
     let res = UserMetadata::query(&dynamo.client, user_pk).await?;
     Ok(Json(UserDetailResponse::from(res)))
 }
