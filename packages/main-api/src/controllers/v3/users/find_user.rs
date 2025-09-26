a00b4d382bc0203867cf771516579803e32004ec
ryancho-0 <ryan@biyard.co>
2025-09-26 12:44:44 +0900

    fix(main-api): revert find_user

--- packages/main-api/src/controllers/v3/users/find_user.rs
+++ packages/main-api/src/controllers/v3/users/find_user.rs
@@ -1,129 +1,80 @@
 use crate::{
-    AppState, Error2, config,
-    models::user::{
-        User, UserOAuth, UserOAuthQueryOption, UserPrincipal, UserPrincipalQueryOption,
-    },
-    types::Provider,
-    utils::{dynamo_extractor::get_principal_from_auth, firebase, password::verify_password},
+    AppState, Error2,
+    models::user::{User, UserDetailResponse, UserMetadata, UserPhoneNumber},
 };
 use bdk::prelude::*;
-
 use dto::{
     JsonSchema, aide,
-    by_axum::{
-        auth::{Authorization, DYNAMO_USER_SESSION_KEY, DynamoUserSession, generate_jwt},
-        axum::{
-            Extension,
-            extract::{Json, State},
-            http::{HeaderMap, header::SET_COOKIE},
-        },
+    by_axum::axum::{
+        Json,
+        extract::{Query, State},
     },
-    by_types::Claims,
 };
 use serde::Deserialize;
-use tower_sessions::Session;
 
 #[derive(Debug, Clone, Deserialize, aide::OperationIo, JsonSchema)]
-pub enum LoginRequest {
-    Email { email: String, password: String },
-    OAuth { provider: Provider, token: String },
-    Telegram { telegram_raw: String },
+pub struct FindUserQueryParams {
+    #[schemars(description = "query type")]
+    pub r#type: FindUserQueryType,
+    #[schemars(description = "query value")]
+    pub value: String,
 }
 
-pub type LoginResponse = (HeaderMap, ());
+#[derive(Debug, Clone, Deserialize, JsonSchema)]
+pub enum FindUserQueryType {
+    Email,
+    Username,
+    PhoneNumber,
+}
+pub type FindUserResponse = UserDetailResponse;
 
-pub async fn login_handler(
+pub async fn find_user_handler(
     State(AppState { dynamo, .. }): State<AppState>,
-    Extension(session): Extension<Session>,
-    Extension(auth): Extension<Option<Authorization>>,
-    Json(req): Json<LoginRequest>,
-) -> Result<(HeaderMap, ()), Error2> {
-    let user = match req {
-        LoginRequest::Email { email, password } => {
-            let (u, _) = User::find_by_email(&dynamo.client, &email, Default::default()).await?;
-            let user = u
-                .get(0)
-                .cloned()
-                .ok_or(Error2::BadRequest("Invalid email or password".into()))?;
-            let hashed_password = user
-                .password
-                .as_ref()
-                .ok_or(Error2::BadRequest("Invalid email or password".into()))?;
-            if !verify_password(&password, &hashed_password).map_err(|e| {
-                Error2::InternalServerError(format!("Password verification error: {}", e))
-            })? {
-                return Err(Error2::BadRequest("Invalid email or password".into()));
+    Query(params): Query<FindUserQueryParams>,
+) -> Result<Json<FindUserResponse>, Error2> {
+    let user_pk = match params.r#type {
+        FindUserQueryType::Email => {
+            let email = params.value;
+            let (users, _bookmark) =
+                User::find_by_email(&dynamo.client, &email, Default::default()).await?;
+            if users.len() == 0 {
+                return Err(Error2::NotFound(format!(
+                    "User not found with email: {}",
+                    email
+                )));
             }
-            user
+            users.into_iter().nth(0).unwrap().pk
         }
-        LoginRequest::OAuth { provider, token } => match provider {
-            Provider::Google => {
-                let uid = firebase::oauth::verify_token(&token).await?;
-
-                // For Migration from Principal Login to Google OAuth.
-                // Remove this code after the users have logged in with Google OAuth.
-                if let Ok(principal) = get_principal_from_auth(auth) {
-                    let (user_principal, _) = UserPrincipal::find_by_principal(
-                        &dynamo.client,
-                        principal,
-                        UserPrincipalQueryOption::builder(),
-                    )
-                    .await?;
-                    let user = user_principal
-                        .get(0)
-                        .cloned()
-                        .ok_or(Error2::Unauthorized("Invalid email or password".into()))?;
-                    UserOAuth::new(user.pk, Provider::Google, uid.clone())
-                        .create(&dynamo.client)
-                        .await?;
-                }
-                let (u, _) = UserOAuth::find_by_provider_and_uid(
-                    &dynamo.client,
-                    uid,
-                    UserOAuthQueryOption::builder().sk(token),
-                )
-                .await?;
-                let user_oauth = u
-                    .get(0)
-                    .cloned()
-                    .ok_or(Error2::NotFound("Invalid OAuth token".into()))?;
-                User::get(&dynamo.client, user_oauth.pk, None::<String>)
-                    .await?
-                    .ok_or(Error2::NotFound("User not found".into()))?
+        FindUserQueryType::Username => {
+            let username = params.value;
+            let (users, _bookmark) =
+                User::find_by_username(&dynamo.client, &username, Default::default()).await?;
+            if users.len() == 0 {
+                return Err(Error2::NotFound(format!(
+                    "User not found with username: {}",
+                    username
+                )));
             }
-        },
-        LoginRequest::Telegram { .. } => {
-            // Handle Telegram login
-            // Not implemented yet
-            return Err(Error2::BadRequest("Telegram login not implemented".into()));
+            users.into_iter().nth(0).unwrap().pk
+        }
+        FindUserQueryType::PhoneNumber => {
+            let phone_number = params.value;
+            let (users, _bookmark) = UserPhoneNumber::find_by_phone_number(
+                &dynamo.client,
+                &phone_number,
+                Default::default(),
+            )
+            .await?;
+            if users.len() == 0 {
+                return Err(Error2::NotFound(format!(
+                    "User not found with phone number: {}",
+                    phone_number
+                )));
+            }
+            users.into_iter().nth(0).unwrap().pk
         }
     };
 
-    let user_session = DynamoUserSession {
-        pk: user.pk.to_string(),
-        typ: user.user_type as i64,
-    };
-    session
-        .insert(DYNAMO_USER_SESSION_KEY, user_session)
-        .await?;
-
-    let mut claims = Claims {
-        sub: user.pk.to_string(),
-        ..Default::default()
-    };
-    let token = generate_jwt(&mut claims)
-        .map_err(|e| Error2::InternalServerError(format!("JWT generation error: {}", e)))?;
-    let mut headers = HeaderMap::new();
-    headers.insert(
-        SET_COOKIE,
-        format!(
-            "{}_auth_token={}; SameSite=Lax; Path=/; Max-Age=2586226; HttpOnly; Secure;",
-            config::get().env,
-            token,
-        )
-        .parse()
-        .unwrap(),
-    );
-
-    Ok((headers, ()))
+    let res = UserMetadata::query(&dynamo.client, user_pk).await?;
+    Ok(Json(UserDetailResponse::from(res)))
 }
