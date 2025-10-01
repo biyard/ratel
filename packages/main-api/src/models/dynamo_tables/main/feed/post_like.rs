d8d2c466b1b1cf9c73d457d218e924a15405e6e9
hackartists <hackartists@gmail.com>
2025-10-01 14:21:56 +0900

    refactor(tests): remove unused test cases and commented code
    - Removed unused test cases in `tests.rs` to clean up the codebase.
    - Deleted commented-out imports and setup code for better readability.
    - Updated `post_like.rs` to uncomment relevant DynamoDB annotations and remove obsolete commented-out code.

--- packages/main-api/src/models/dynamo_tables/main/feed/post_like.rs
+++ packages/main-api/src/models/dynamo_tables/main/feed/post_like.rs
@@ -6,9 +6,9 @@ pub struct PostLike {
     pub pk: Partition,
     pub sk: EntityType,
 
-    // #[dynamo(prefix = "LIKE", name = "find_by_user", index = "gsi1", pk)]
+    #[dynamo(prefix = "LIKE", name = "find_by_user", index = "gsi1", pk)]
     pub user_pk: Partition,
-    // #[dynamo(index = "gsi1", sk)]
+    #[dynamo(index = "gsi1", sk)]
     pub created_at: i64,
 }
 
@@ -24,53 +24,3 @@ impl PostLike {
         }
     }
 }
-
-// impl PostLike {
-//     pub async fn batch_get(
-//         cli: &aws_sdk_dynamodb::Client,
-//         keys: Vec<(impl std::fmt::Display, impl std::fmt::Display)>,
-//     ) -> Result<Vec<Self>, crate::Error2> {
-//         let keys = keys
-//             .iter()
-//             .map(|key| {
-//                 std::collections::HashMap::from([
-//                     (
-//                         Self::pk_field(),
-//                         aws_sdk_dynamodb::types::AttributeValue::S(key.0.to_string()),
-//                     ),
-//                     (
-//                         "sk".to_string(),
-//                         aws_sdk_dynamodb::types::AttributeValue::S(key.1.to_string()),
-//                     ),
-//                 ])
-//             })
-//             .collect::<Vec<_>>();
-
-//         let keys_and_attributes = aws_sdk_dynamodb::types::KeysAndAttributes::builder()
-//             .set_keys(Some(keys))
-//             .consistent_read(false)
-//             .build()
-//             .map_err(Into::<aws_sdk_dynamodb::Error>::into)?;
-
-//         let table_name = Self::table_name();
-
-//         let response = cli
-//             .batch_get_item()
-//             .request_items(table_name, keys_and_attributes)
-//             .send()
-//             .await
-//             .map_err(Into::<aws_sdk_dynamodb::Error>::into)?;
-
-//         let items = if let Some(responses) = response.responses() {
-//             if let Some(items) = responses.get(table_name) {
-//                 serde_dynamo::from_items(items.to_vec())?
-//             } else {
-//                 vec![]
-//             }
-//         } else {
-//             vec![]
-//         };
-
-//         Ok(items)
-//     }
-// }
