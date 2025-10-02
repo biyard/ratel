7e8ab70060ecd469cf1ece1b33335b46a8808321
hackartists <hackartists@gmail.com>
2025-10-02 11:58:16 +0900

    feat(tests): enhance post endpoint tests and simplify macros
    - Added logging for create and get post responses in tests
    - Updated `PostDetailResponse` struct to remove unused `#[serde(flatten)]`
    - Simplified and removed redundant macro definitions for HTTP requests

--- packages/main-api/src/controllers/v3/posts/tests.rs
+++ packages/main-api/src/controllers/v3/posts/tests.rs
@@ -9,20 +9,28 @@ use crate::*;
 async fn test_create_post_by_user() {
     let TestContextV3 { app, test_user, .. } = TestContextV3::setup().await;
 
-    let (status, headers, body) = post! {
+    let (status, _headers, body) = post! {
         app: app,
         path: "/v3/posts",
-        headers: test_user.1,
+        headers: test_user.1.clone(),
         response_type: CreatePostResponse
     };
 
     assert_eq!(status, 200);
     assert!(body.post_pk.to_string().len() > 0);
 
+    tracing::info!("Create post response pk: {:?}", body.post_pk);
+
+    let path = format!("/v3/posts/{}", body.post_pk.to_string()).replace("#", "%23");
+
     let (status, _headers, body) = get! {
         app: app,
-        path: format!("/v3/posts/{}", body.post_pk.to_string())
+        path: path,
+        headers: test_user.1,
+        response_type: PostDetailResponse
     };
+    tracing::info!("Get post response: {:?}", body);
+    assert_eq!(status, 200);
 }
 
 #[tokio::test]
