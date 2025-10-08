95b7809b3e7256544cc20261653e13e3e4501041
hackartists <hackartists@gmail.com>
2025-10-08 13:06:13 +0900

    feat(main-api): integrate web assets and enhance build process
    - Added support for serving web assets in `main-api` via `dist` directory
    - Updated `build.rs` to copy web assets and set environment variables for CSS/JS
    - Introduced new `index.html` template for dynamic asset injection
    - Enhanced `Makefile` and `Dockerfile` to include web build steps
    - Updated `docker-compose.yaml` with new environment variables for web integration
    - Modified `api-entrypoint.sh` to install Node.js and package managers
    - Adjusted `ts-packages/web` metadata and assets for consistency with `main-api`

--- packages/main-api/src/controllers/web/mod.rs
+++ packages/main-api/src/controllers/web/mod.rs
@@ -14,5 +14,20 @@ pub fn route() -> Result<Router, Error> {
     Ok(Router::new()
         .native_route("/", axum::native_routing::get(home_page_handler))
         .nest_service("/assets", get_service(ServeDir::new("dist/assets")))
+        .nest_service(
+            "/favicon.ico",
+            get_service(ServeDir::new("dist/favicon.ico")),
+        )
+        .nest_service("/images", get_service(ServeDir::new("dist/images")))
+        .nest_service("/animations", get_service(ServeDir::new("dist/animations")))
+        .nest_service("/documents", get_service(ServeDir::new("dist/documents")))
+        .nest_service("/logos", get_service(ServeDir::new("dist/logos")))
+        .nest_service("/sounds", get_service(ServeDir::new("dist/sounds")))
+        .nest_service("/videos", get_service(ServeDir::new("dist/videos")))
+        .nest_service(
+            "/tailwind.css",
+            get_service(ServeDir::new("dist/tailwind.css")),
+        )
+        .nest_service("/main.css", get_service(ServeDir::new("dist/main.css")))
         .with_state(client))
 }
