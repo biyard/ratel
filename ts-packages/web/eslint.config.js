99d2d62a896c31cabe83dc81981cd98f703a6092
hackartists <hackartists@gmail.com>
2025-10-11 16:39:47 +0900

    feat(eslint): enhance configuration with Prettier and unused imports rules
    - Added Prettier and unused imports plugins to ESLint configuration
    - Configured Prettier rules to enforce code formatting standards
    - Enabled rules for detecting and removing unused imports and variables
    - Integrated Storybook ESLint configuration for better compatibility
    - Refactored imports in `use-thread-controller.tsx` for consistency and clarity

--- ts-packages/web/eslint.config.js
+++ ts-packages/web/eslint.config.js
@@ -1,12 +1,28 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+import storybook from 'eslint-plugin-storybook';
+import prettierPlugin from 'eslint-plugin-prettier'; // Added import
+import unusedImports from 'eslint-plugin-unused-imports';
+
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
 export default defineConfig([
   globalIgnores(['dist']),
+  ...storybook.configs['flat/recommended'],
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -20,4 +36,36 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
-])
+
+  // ESLint plugin Prettier configuration
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      prettier: prettierPlugin,
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      ...prettierPlugin.configs.recommended.rules,
+      'prettier/prettier': 'error',
+      'react/no-unescaped-entities': [
+        'error',
+        {
+          forbid: ['>', '}', '"'],
+        },
+      ],
+      'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'error',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+
+  ...compat.extends('prettier'),
+]);
