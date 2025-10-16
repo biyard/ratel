295c240f5253197664435f0fa2325bf04fdc2abd
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2025-10-16 18:49:15 +0900

    fix deliberation model location

---
+++ packages/main-api/src/models/dynamo_tables/main/space/space_discussion.rs
@@ -0,0 +1,112 @@
+use crate::{
+    models::space::{DiscussionMemberResponse, DiscussionParticipantResponse},
+    types::*,
+};
+
+use bdk::prelude::*;
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, Default, JsonSchema)]
+pub struct SpaceDiscussionCreateRequest {
+    pub discussion_pk: Option<String>,
+    pub started_at: i64,
+    pub ended_at: i64,
+
+    pub name: String,
+    pub description: String,
+    pub user_ids: Vec<Partition>,
+}
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, DynamoEntity, Default)]
+pub struct SpaceDiscussion {
+    pub pk: Partition,
+    pub sk: EntityType,
+    pub started_at: i64,
+    pub ended_at: i64,
+
+    pub name: String,
+    pub description: String,
+    pub meeting_id: Option<String>,
+    pub pipeline_id: String,
+
+    pub media_pipeline_arn: Option<String>,
+    pub record: Option<String>,
+}
+
+impl SpaceDiscussion {
+    pub fn new(
+        space_pk: Partition,
+        name: String,
+        description: String,
+        started_at: i64,
+        ended_at: i64,
+        meeting_id: Option<String>,
+        pipeline_id: String,
+        media_pipeline_arn: Option<String>,
+        record: Option<String>,
+    ) -> Self {
+        let uid = uuid::Uuid::new_v4().to_string();
+
+        Self {
+            pk: space_pk,
+            sk: EntityType::Discussion(uid),
+            started_at,
+            ended_at,
+
+            name,
+            description,
+            meeting_id,
+            pipeline_id,
+
+            media_pipeline_arn,
+            record,
+        }
+    }
+}
+
+#[derive(
+    Debug,
+    Clone,
+    Default,
+    serde::Serialize,
+    serde::Deserialize,
+    schemars::JsonSchema,
+    aide::OperationIo,
+)]
+pub struct SpaceDiscussionResponse {
+    pub pk: Partition,
+
+    pub started_at: i64,
+    pub ended_at: i64,
+
+    pub name: String,
+    pub description: String,
+    pub meeting_id: Option<String>,
+    pub pipeline_id: String,
+
+    pub media_pipeline_arn: Option<String>,
+    pub record: Option<String>,
+
+    pub members: Vec<DiscussionMemberResponse>,
+    pub participants: Vec<DiscussionParticipantResponse>,
+}
+
+impl From<SpaceDiscussion> for SpaceDiscussionResponse {
+    fn from(discussion: SpaceDiscussion) -> Self {
+        Self {
+            pk: match discussion.sk {
+                EntityType::Discussion(v) => Partition::Discussion(v.to_string()),
+                _ => Partition::Discussion("".to_string()),
+            },
+            started_at: discussion.started_at,
+            ended_at: discussion.ended_at,
+            name: discussion.name,
+            description: discussion.description,
+            meeting_id: discussion.meeting_id,
+            pipeline_id: discussion.pipeline_id,
+            media_pipeline_arn: discussion.media_pipeline_arn,
+            record: discussion.record,
+            members: vec![],
+            participants: vec![],
+        }
+    }
+}
---
+++ packages/main-api/src/models/dynamo_tables/main/space/space_discussion_participant.rs
@@ -0,0 +1,93 @@
+use crate::{models::user::User, types::*};
+use bdk::prelude::*;
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, DynamoEntity, Default)]
+pub struct SpaceDiscussionParticipant {
+    pub pk: Partition, //discussion_pk
+    #[dynamo(index = "gsi1", sk)]
+    pub sk: EntityType, //participant id
+
+    pub participant_id: Option<String>,
+
+    #[dynamo(prefix = "USER_PK", name = "find_by_user_pk", index = "gsi1", pk)]
+    pub user_pk: Partition,
+    pub author_display_name: String,
+    pub author_profile_url: String,
+    pub author_username: String,
+}
+
+impl SpaceDiscussionParticipant {
+    pub fn new(
+        discussion_pk: Partition,
+
+        participant_id: String,
+        User {
+            pk,
+            display_name,
+            profile_url,
+            username,
+            ..
+        }: User,
+    ) -> Self {
+        Self {
+            pk: discussion_pk,
+            sk: EntityType::DiscussionParticipant(participant_id.clone()),
+            participant_id: Some(participant_id),
+            user_pk: pk,
+            author_display_name: display_name,
+            author_profile_url: profile_url,
+            author_username: username,
+        }
+    }
+
+    pub fn id(&self) -> String {
+        if let Some(id) = &self.participant_id {
+            return id.clone();
+        }
+        if let EntityType::DiscussionParticipant(v) = &self.sk {
+            return v.clone();
+        }
+        String::new()
+    }
+
+    pub fn discussion_user_pk_or_compute(&self) -> Partition {
+        let discussion_id = match &self.pk {
+            Partition::Discussion(v) => v.as_str(),
+            _ => "",
+        };
+        let user_id = match &self.user_pk {
+            Partition::User(v) | Partition::Team(v) => v.as_str(),
+            _ => "",
+        };
+        Partition::DiscussionUser(format!("{discussion_id}#{user_id}"))
+    }
+}
+
+#[derive(
+    Debug,
+    Clone,
+    Default,
+    serde::Serialize,
+    serde::Deserialize,
+    schemars::JsonSchema,
+    aide::OperationIo,
+)]
+pub struct SpaceDiscussionParticipantResponse {
+    pub user_pk: Partition,
+    pub author_display_name: String,
+    pub author_profile_url: String,
+    pub author_username: String,
+    pub participant_id: String,
+}
+
+impl From<SpaceDiscussionParticipant> for SpaceDiscussionParticipantResponse {
+    fn from(p: SpaceDiscussionParticipant) -> Self {
+        Self {
+            user_pk: p.clone().user_pk,
+            author_display_name: p.clone().author_display_name,
+            author_profile_url: p.clone().author_profile_url,
+            author_username: p.clone().author_username,
+            participant_id: p.id(),
+        }
+    }
+}
