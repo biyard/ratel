a1e6d6c65fbd7bcd6d9353ae2e38373b030fb704
ryancho-0 <ryan@biyard.co>
2025-10-20 15:47:07 +0900

    feat(main-api): add route

--- packages/main-api/src/controllers/v3/spaces/sprint_leagues/mod.rs
+++ packages/main-api/src/controllers/v3/spaces/sprint_leagues/mod.rs
@@ -8,3 +8,17 @@ pub use vote_sprint_league::*;
 
 #[cfg(test)]
 pub mod tests;
+
+use crate::AppState;
+use bdk::prelude::*;
+use by_axum::aide::axum::routing::*;
+use by_axum::axum::*;
+
+pub fn route() -> Router<AppState> {
+    Router::new()
+        .route(
+            "/",
+            get(get_sprint_league_handler).put(upsert_sprint_league_handler),
+        )
+        .route("/vote", post(vote_sprint_league_handler))
+}
