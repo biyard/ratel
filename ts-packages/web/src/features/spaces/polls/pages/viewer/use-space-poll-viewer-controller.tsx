ec266b243a3b46bf9328be2d5b4f92280f1b3b16
hackartists <hackartists@gmail.com>
2025-10-21 15:47:34 +0900

    feat(spaces): enhance space publishing and viewer functionality
    - Refactored `update_space_handler` in Rust backend to streamline updates
      with unified transaction handling for space and post updates.
    - Added `PublishSpaceModal` and `usePublishSpaceMutation` to support
      space publishing in the frontend.
    - Updated `SpacePollViewerPage` to include `TimeRangeSetting` and
      `SurveyViewer` components for improved poll viewing.
    - Enhanced `SpacePollViewerController` to manage poll answers and
      integrate with new hooks.
    - Adjusted `SpaceVisibility` type to support case-insensitive values.
    - Updated API endpoints for publishing spaces to use `PATCH` instead of
      `POST`.

--- ts-packages/web/src/features/spaces/polls/pages/viewer/use-space-poll-viewer-controller.tsx
+++ ts-packages/web/src/features/spaces/polls/pages/viewer/use-space-poll-viewer-controller.tsx
@@ -1,16 +1,39 @@
-import { useState } from 'react';
+import { useSpaceById } from '@/features/spaces/hooks/use-space-by-id';
+import { Space } from '@/features/spaces/types/space';
+import { TFunction } from 'i18next';
+import usePollSpace from '../../hooks/use-poll-space';
+import { useTranslation } from 'react-i18next';
+import { Poll } from '../../types/poll';
+import { SurveyAnswer } from '../../types/poll-question';
+import { logger } from '@/lib/logger';
 import { State } from '@/types/state';
+import { useState } from 'react';
 
 export class SpacePollViewerController {
   constructor(
-    /* public data: ReturnType<typeof useSpacePollViewerData>, */
-    public state: State<boolean>,
+    public space: Space,
+    public poll: Poll,
+    public t: TFunction<'SpaceSurvey', undefined>,
+    public answers: State<Record<number, SurveyAnswer>>,
   ) {}
+
+  handleUpdateAnswer = (questionIdx: number, answer: SurveyAnswer) => {
+    logger.debug(
+      `handleUpdateAnswer called for questionIdx ${questionIdx}`,
+      answer,
+    );
+    const currentAnswers = this.answers.get();
+    currentAnswers[questionIdx] = answer;
+    this.answers.set({ ...currentAnswers });
+  };
 }
 
-export function useSpacePollViewerController() {
-  // TODO: use or define hooks
-  const state = useState(false);
+export function useSpacePollViewerController(spacePk, pollPk) {
+  const { data: space } = useSpaceById(spacePk);
+  const { data: poll } = usePollSpace(spacePk, pollPk);
+  const { t } = useTranslation('SpaceSurvey');
+  // FIXME: This should be my current answers
+  const answers = useState<Record<number, SurveyAnswer>>({});
 
-  return new SpacePollViewerController(new State(state));
+  return new SpacePollViewerController(space, poll, t, new State(answers));
 }
