0d47e69651ec49b211fd525f743c26eb0bee3d74
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2025-10-21 19:25:12 +0900

    impelement discussion crud method to ui not testing

---
+++ ts-packages/web/src/features/spaces/discussions/hooks/use-delete-discussion-mutation.ts
@@ -0,0 +1,30 @@
+import { spaceKeys } from '@/constants';
+import { optimisticUpdate } from '@/lib/hook-utils';
+import { useMutation } from '@tanstack/react-query';
+import { SpaceDiscussionResponse } from '../types/space-discussion-response';
+import { deleteSpaceDiscussion } from '@/lib/api/ratel/discussion.spaces.v3';
+
+export function useDeleteDiscussionMutation<
+  T extends SpaceDiscussionResponse,
+>() {
+  const mutation = useMutation({
+    mutationKey: ['delete-discussion'],
+    mutationFn: async ({
+      spacePk,
+      discussionPk,
+    }: {
+      spacePk: string;
+      discussionPk: string;
+    }) => {
+      await deleteSpaceDiscussion(spacePk, discussionPk);
+    },
+    onSuccess: async (_, { spacePk }) => {
+      const spaceQK = spaceKeys.discussion(spacePk);
+      await optimisticUpdate<T>({ queryKey: spaceQK }, (response) => {
+        return response;
+      });
+    },
+  });
+
+  return mutation;
+}
