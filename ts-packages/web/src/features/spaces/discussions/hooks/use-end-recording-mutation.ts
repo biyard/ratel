0b8320fd9c326367c6a03713bd6dc930dada4941
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2025-10-22 17:02:53 +0900

    add discussion hook mutation

---
+++ ts-packages/web/src/features/spaces/discussions/hooks/use-end-recording-mutation.ts
@@ -0,0 +1,28 @@
+import { spaceKeys } from '@/constants';
+import { optimisticUpdate } from '@/lib/hook-utils';
+import { useMutation } from '@tanstack/react-query';
+import { SpaceDiscussionResponse } from '../types/space-discussion-response';
+import { discussionEndRecording } from '@/lib/api/ratel/discussion.spaces.v3';
+
+export function useEndRecordingMutation<T extends SpaceDiscussionResponse>() {
+  const mutation = useMutation({
+    mutationKey: ['end-recording'],
+    mutationFn: async ({
+      spacePk,
+      discussionPk,
+    }: {
+      spacePk: string;
+      discussionPk: string;
+    }) => {
+      await discussionEndRecording(spacePk, discussionPk);
+    },
+    onSuccess: async (_, { spacePk, discussionPk }) => {
+      const discussionQk = spaceKeys.discussion(spacePk, discussionPk);
+      await optimisticUpdate<T>({ queryKey: discussionQk }, (response) => {
+        return response;
+      });
+    },
+  });
+
+  return mutation;
+}
