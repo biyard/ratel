d29b72549d515e4ad52d1d034c510559c990c5bd
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2025-10-22 11:45:04 +0900

    implement discussion modal not testing

---
+++ ts-packages/web/src/features/spaces/discussions/pages/creator/i18n.tsx
@@ -0,0 +1,30 @@
+const en = {
+  discussions: 'discussions',
+  add_discussion: 'Add Discussion',
+  select_space_type: 'New Discussion',
+
+  title: 'Title',
+  description: 'Description',
+  date: 'Date',
+  title_hint: 'input your discussion name.',
+  description_hint: 'What is the purpose of your discussion?',
+};
+
+const ko = {
+  discussions: '토론',
+  add_discussion: '토론 추가하기',
+  select_space_type: '토론 추가하기',
+
+  title: '제목',
+  description: '설명',
+  date: '날짜',
+  title_hint: '토론 제목을 입력해주세요.',
+  description_hint: '토론의 목적은 무엇인가요?',
+};
+
+const i18n = {
+  en,
+  ko,
+};
+
+export default i18n;
--- ts-packages/web/src/features/spaces/discussions/pages/creator/space-discussion-editor-page.tsx
+++ ts-packages/web/src/features/spaces/discussions/pages/creator/space-discussion-editor-page.tsx
@@ -1,17 +1,27 @@
 import { logger } from '@/lib/logger';
 import { SpaceDiscussionPathProps } from '../space-discussion-path-props';
 import { useSpaceDiscussionEditorController } from './use-space-discussion-editor-controller';
+import { Col } from '@/components/ui/col';
+import DiscussionEditor from '../../components/discussion-editor';
 
 export function SpaceDiscussionEditorPage({
   spacePk,
 }: SpaceDiscussionPathProps) {
   logger.debug(`SpaceDiscussionEditorPage: spacePk=${spacePk}`);
 
-  const _ctrl = useSpaceDiscussionEditorController(spacePk);
+  const ctrl = useSpaceDiscussionEditorController(spacePk);
 
   return (
     <>
-      <div className="text-text-primary">discussion editor</div>
+      <Col>
+        <Col>
+          <DiscussionEditor
+            t={ctrl.t}
+            onadd={ctrl.handleAddDiscussion}
+            canEdit={true}
+          />
+        </Col>
+      </Col>
     </>
   );
 }
