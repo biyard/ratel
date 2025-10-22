066fede416c182ef95dc837fa6024838a6947f65
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2025-10-22 14:41:01 +0900

    fix discussion viewer

--- ts-packages/web/src/features/spaces/discussions/pages/viewer/use-space-discussion-viewer-controller.tsx
+++ ts-packages/web/src/features/spaces/discussions/pages/viewer/use-space-discussion-viewer-controller.tsx
@@ -1,27 +1,35 @@
-import useSpaceById from '@/hooks/use-space-by-id';
-import { Space } from '@/lib/api/models/spaces';
 import useDiscussionSpace from '../../hooks/use-discussion-space';
 import { SpaceDiscussionResponse } from '../../types/space-discussion-response';
+import { useTranslation } from 'react-i18next';
+import { TFunction } from 'i18next';
+import { useSpaceById } from '@/features/spaces/hooks/use-space-by-id';
+import { Space } from '@/features/spaces/types/space';
+import { ListDiscussionResponse } from '../../types/list-discussion-response';
 
 export class SpaceDiscussionViewerController {
   constructor(
     public spacePk: string,
     public space: Space,
+    public discussion: ListDiscussionResponse,
     public bookmark: string | null | undefined,
     public discussions: SpaceDiscussionResponse[],
+    public t: TFunction<'SpaceDiscussionEditor', undefined>,
   ) {}
 }
 
-export function useSpaceDiscussionViewerController(spacePk) {
+export function useSpaceDiscussionViewerController(spacePk: string) {
   const { data: space } = useSpaceById(spacePk);
   const { data: discussion } = useDiscussionSpace(spacePk);
   const bookmark = discussion.bookmark;
   const discussions = discussion.discussions;
+  const { t } = useTranslation('SpaceDiscussionEditor');
 
   return new SpaceDiscussionViewerController(
     spacePk,
     space,
+    discussion,
     bookmark,
     discussions,
+    t,
   );
 }
