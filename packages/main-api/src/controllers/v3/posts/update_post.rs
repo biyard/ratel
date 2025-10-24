74e93ccb39f46cae5ac13f2c11e3a33a67cb9318
ryancho-0 <ryan@biyard.co>
2025-10-24 15:00:38 +0900

    Revert "feat(main-api): update `update_post` handler for support change post type, and artwork_metadata"
    This reverts commit 5caa3d34dd7765dcfab8ac90ebed1af92abe5acf.

--- packages/main-api/src/controllers/v3/posts/update_post.rs
+++ packages/main-api/src/controllers/v3/posts/update_post.rs
@@ -1,9 +1,8 @@
 use crate::models::feed::Post;
 use crate::models::user::User;
-use crate::models::{PostArtwork, PostArtworkMetadata};
-use crate::types::{EntityType, PostStatus, PostType, TeamGroupPermission, Visibility};
+use crate::types::{PostStatus, TeamGroupPermission, Visibility};
 use crate::utils::validator::{validate_content, validate_title};
-use crate::{AppState, Error2, transact_write_items};
+use crate::{AppState, Error2};
 use aide::NoApi;
 use axum::extract::{Json, Path, State};
 use bdk::prelude::*;
@@ -18,9 +17,6 @@ pub enum UpdatePostRequest {
         publish: bool,
         visibility: Option<Visibility>,
     },
-    PostType {
-        r#type: PostType,
-    },
     Writing {
         title: String,
         content: String,
@@ -28,15 +24,10 @@ pub enum UpdatePostRequest {
     Image {
         images: Vec<String>,
     },
-
     Info {
         visibility: Visibility,
     },
-    WritingArtwork {
-        title: String,
-        content: String,
-        metadata: Vec<PostArtworkMetadata>,
-    },
+    // TODO: Artwork metadata
 }
 
 pub async fn update_post_handler(
@@ -62,7 +53,7 @@ pub async fn update_post_handler(
     let updater = Post::updater(&post.pk, &post.sk).with_updated_at(now);
     post.updated_at = now;
 
-    let transacts = match req {
+    let req = match req {
         UpdatePostRequest::Writing { title, content } => {
             validate_title(&title)?;
             validate_content(&content)?;
@@ -70,20 +61,15 @@ pub async fn update_post_handler(
             post.title = title.clone();
             post.html_contents = content.clone();
 
-            vec![
-                updater
-                    .with_title(title)
-                    .with_html_contents(content)
-                    .transact_write_item(),
-            ]
+            updater.with_title(title).with_html_contents(content)
         }
         UpdatePostRequest::Image { images } => {
             post.urls = images.clone();
-            vec![updater.with_urls(images).transact_write_item()]
+            updater.with_urls(images)
         }
         UpdatePostRequest::Info { visibility } => {
             post.visibility = Some(visibility.clone());
-            vec![updater.with_visibility(visibility).transact_write_item()]
+            updater.with_visibility(visibility)
         }
         UpdatePostRequest::Publish {
             publish,
@@ -114,56 +100,15 @@ pub async fn update_post_handler(
 
             post.status = PostStatus::Published;
 
-            vec![
-                updater
-                    .with_status(PostStatus::Published)
-                    .with_title(title)
-                    .with_html_contents(content)
-                    .with_visibility(visibility)
-                    .transact_write_item(),
-            ]
-        }
-        UpdatePostRequest::PostType { r#type } => {
-            post.post_type = r#type.clone();
-            vec![updater.with_post_type(r#type).transact_write_item()]
-        }
-        UpdatePostRequest::WritingArtwork {
-            title,
-            content,
-            metadata,
-        } => {
-            validate_title(&title)?;
-            validate_content(&content)?;
-
-            let mut transacts = vec![];
-            post.title = title.clone();
-            post.html_contents = content.clone();
-            transacts.push(
-                updater
-                    .with_title(title)
-                    .with_html_contents(content)
-                    .transact_write_item(),
-            );
-
-            let next_metadata =
-                PostArtwork::get(cli, &post.pk, Some(EntityType::PostArtwork)).await?;
-            let _metadata = if let Some(mut meta) = next_metadata {
-                let artwork_updater =
-                    PostArtwork::updater(post.pk.clone(), EntityType::PostArtwork)
-                        .with_metadata(metadata.clone());
-                transacts.push(artwork_updater.transact_write_item());
-                meta.metadata = metadata;
-                meta
-            } else {
-                let next_metadata = PostArtwork::new(post.pk.clone(), metadata);
-                transacts.push(next_metadata.create_transact_write_item());
-                next_metadata
-            };
-            transacts
+            updater
+                .with_status(PostStatus::Published)
+                .with_title(title)
+                .with_html_contents(content)
+                .with_visibility(visibility)
         }
     };
 
-    transact_write_items!(cli, transacts)?;
+    req.execute(cli).await?;
 
     Ok(Json(post))
 }
