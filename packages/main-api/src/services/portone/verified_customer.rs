60d7822352feae2fd0f586d048928fbce7571b14
hackartists <hackartists@gmail.com>
2025-10-28 22:38:09 +0900

    feat(verified_customer): add VerifiedGender enum for gender field
    - Replaced `gender` field type in `VerifiedCustomer` with `VerifiedGender`
    - Introduced `VerifiedGender` enum with variants: `None`, `Male`, and `Female`
    - Added relevant traits to `VerifiedGender` for serialization, des# [copilot:gpt-4o] Generating commit message...

--- packages/main-api/src/services/portone/verified_customer.rs
+++ packages/main-api/src/services/portone/verified_customer.rs
@@ -4,9 +4,27 @@ use crate::*;
 #[serde(rename_all = "camelCase")]
 pub struct VerifiedCustomer {
     pub birth_date: String,
-    pub gender: String,
+    pub gender: VerifiedGender,
     pub id: String,
     pub is_foreigner: bool,
     pub name: String,
     pub phone_number: String,
+}
+
+#[derive(
+    Debug,
+    Clone,
+    serde_with::SerializeDisplay,
+    serde_with::DeserializeFromStr,
+    Default,
+    DynamoEnum,
+    JsonSchema,
+    OperationIo,
+)]
+pub enum VerifiedGender {
+    #[default]
+    None,
+
+    Male,
+    Female,
 }
