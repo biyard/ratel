7561873843c4cf8b0d1138a35b25692715a3bccb
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2025-10-31 17:09:18 +0900

    add invitation hooks

---
+++ ts-packages/web/src/features/spaces/invitations/hooks/use-invitation.ts
@@ -0,0 +1,29 @@
+import {
+  useSuspenseQuery,
+  UseSuspenseQueryResult,
+} from '@tanstack/react-query';
+
+import { spaceKeys } from '@/constants';
+import { call } from '@/lib/api/ratel/call';
+import { ListInvitationMemberResponse } from '../types/list-invitation-member-response';
+
+export function getOption(spacePk: string) {
+  return {
+    queryKey: spaceKeys.invitations(spacePk),
+    queryFn: async () => {
+      const discussion = await call(
+        'GET',
+        `/v3/spaces/${encodeURIComponent(spacePk)}/invitations`,
+      );
+      return new ListInvitationMemberResponse(discussion);
+    },
+    refetchOnWindowFocus: false,
+  };
+}
+
+export default function useDiscussion(
+  spacePk: string,
+): UseSuspenseQueryResult<ListInvitationMemberResponse> {
+  const query = useSuspenseQuery(getOption(spacePk));
+  return query;
+}
