d05836da250cfd80ba400cef6814554fce26596a
blikq <blikqmain@gmail.com>
2025-10-31 11:25:05 +0100

    persistence of DID anf endpoints

--- packages/main-api/src/controllers/v3/did/mod.rs
+++ packages/main-api/src/controllers/v3/did/mod.rs
@@ -1,9 +1,26 @@
+pub mod create_did;
+pub mod deactivate_did;
 pub mod reconcil_identity_verification;
+pub mod resolve_did;
+pub mod update_did;
 
+use create_did::create_did_handler;
+use deactivate_did::deactivate_did_handler;
 use reconcil_identity_verification::reconcil_identity_verification_handler;
+use resolve_did::resolve_did_handler;
+use update_did::update_did_handler;
 
 use crate::*;
+use by_axum::aide::axum::routing::*;
 
 pub fn route() -> Result<Router<AppState>> {
-    Ok(Router::new().route("/kyc", post(reconcil_identity_verification_handler)))
+    Ok(Router::new()
+        .route("/", post(create_did_handler))
+        .route(
+            "/:did",
+            get(resolve_did_handler)
+                .put(update_did_handler)
+                .delete(deactivate_did_handler),
+        )
+        .route("/kyc", post(reconcil_identity_verification_handler)))
 }
