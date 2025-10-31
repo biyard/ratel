42d3bad6b34586330f1e995b1ee0966379d88695
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2025-10-31 12:01:49 +0900

    implement one question viewer

--- ts-packages/web/src/features/spaces/polls/pages/creator/use-space-poll-editor-controller.tsx
+++ ts-packages/web/src/features/spaces/polls/pages/creator/use-space-poll-editor-controller.tsx
@@ -19,11 +19,14 @@ import { useUpdateResponseEditableMutation } from '../../hooks/use-update-respon
 import { showErrorToast, showSuccessToast } from '@/lib/toast';
 import { NavigateFunction, useNavigate } from 'react-router';
 import { route } from '@/route';
+import { useUserInfo } from '@/hooks/use-user-info';
+import { UserResponse } from '@/lib/api/ratel/users.v3';
 
 export class SpacePollEditorController {
   constructor(
     public space: Space,
     public poll: Poll,
+    public user: UserResponse | null,
     public navigate: NavigateFunction,
     public questions: State<PollQuestion[]>,
     public t: TFunction<'SpaceSurvey', undefined>,
@@ -138,6 +141,7 @@ export class SpacePollEditorController {
 export function useSpacePollEditorController(spacePk: string, pollPk: string) {
   const { data: space } = useSpaceById(spacePk);
   const { data: poll } = usePollSpace(spacePk, pollPk);
+  const { data: user } = useUserInfo();
   const navigator = useNavigate();
   const questions = useState(poll.questions || []);
   const { t } = useTranslation('SpaceSurvey');
@@ -152,6 +156,7 @@ export function useSpacePollEditorController(spacePk: string, pollPk: string) {
   return new SpacePollEditorController(
     space,
     poll,
+    user,
     navigator,
     new State(questions),
     t,
--- ts-packages/web/src/features/spaces/polls/pages/viewer/space-poll-viewer-i18n.tsx
+++ ts-packages/web/src/features/spaces/polls/pages/viewer/space-poll-viewer-i18n.tsx
@@ -1,12 +1,16 @@
 export const i18nSpacePollViewerPage = {
   en: {
     btn_submit: 'Submit',
+    btn_prev: 'Prev',
+    btn_next: 'Next',
     btn_back: 'Back',
     btn_update: 'Update',
     btn_login: 'Log in to Submit',
   },
   ko: {
     btn_submit: '제출',
+    btn_prev: '이전',
+    btn_next: '다음',
     btn_back: '뒤로가기',
     btn_update: '수정',
     btn_login: '로그인 후 제출',
