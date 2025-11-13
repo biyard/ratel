0512e3b2d7b61744a5b2aecad32619121d9553f6
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2025-11-13 14:12:28 +0900

    implement team invite mail

--- packages/main-api/src/utils/html/mod.rs
+++ packages/main-api/src/utils/html/mod.rs
@@ -1,6 +1,8 @@
 mod create_space_post;
 mod invite_space;
+mod invite_team;
 mod signup;
 pub use create_space_post::create_space_post_html;
 pub use invite_space::invite_space_html;
+pub use invite_team::invite_team_html;
 pub use signup::signup_html;
