4654ff175e30b258051d4b097e9f39a981e6ae36
blikq <blikqmain@gmail.com>
2025-11-14 11:06:18 +0100

    fix: test cases

--- packages/main-api/src/controllers/v3/spaces/panels/mod.rs
+++ packages/main-api/src/controllers/v3/spaces/panels/mod.rs
@@ -3,10 +3,12 @@ mod delete_all_panels;
 mod list_panel_attributes;
 pub mod list_participants;
 pub mod update_panel_quota;
+mod update_space_panels;
 use create_panel_quota::create_panel_quota_handler;
 use list_panel_attributes::list_panel_attributes_handler;
 use list_participants::*;
 use update_panel_quota::*;
+use update_space_panels::update_space_panels_handler;
 
 #[cfg(test)]
 pub mod tests;
@@ -19,6 +21,7 @@ pub fn route() -> Router<AppState> {
             "/",
             post(create_panel_quota_handler)
                 .get(list_panel_attributes_handler)
+                .patch(update_space_panels_handler)
                 .delete(delete_all_panels::delete_all_panels_handler),
         )
         .route("/:panel_sk", patch(update_panel_quota_handler))
