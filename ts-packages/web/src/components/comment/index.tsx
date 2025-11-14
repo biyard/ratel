91e98eb515b6250f653c486d9948d6f7ea4d505e
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2025-11-14 16:25:08 +0900

    implement foldable component

--- ts-packages/web/src/components/comment/index.tsx
+++ ts-packages/web/src/components/comment/index.tsx
@@ -137,6 +137,7 @@ export function Comment({
             content={comment.content}
             editable={false}
             showToolbar={false}
+            isFoldable={true}
           />
         )}
 
