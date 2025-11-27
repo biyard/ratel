5fb85403927a2eb0aa9ba1e3e160be401e47fb9d
ryancho-0 <ryan@biyard.co>
2025-11-28 07:38:51 +0900

    feat(api): enhance Biyard service with user partition handling for awarding points and retrieving balances

--- packages/main-api/src/services/biyard/biyard.rs
+++ packages/main-api/src/services/biyard/biyard.rs
@@ -33,33 +33,111 @@ impl Biyard {
         }
     }
 
+    fn convert_to_meta_user_id(user_pk: &Partition) -> String {
+        match user_pk {
+            Partition::User(id) => id.clone(),
+            _ => panic!("Biyard user_pk must be of Partition::User type"),
+        }
+    }
     pub async fn award_points(
         &self,
-        user_id: &str,
+        user_pk: Partition,
         points: i64,
+        description: String,
         month: Option<String>,
-    ) -> Result<()> {
+    ) -> Result<AwardPointResponse> {
         //TODO: Validate Month format: "YYYY-MM"
 
         let path = format!("{}/projects/{}/points", self.base_url, self.project_id);
         let body = AwardPointRequest {
             tx_type: "Award".to_string(),
-            to: user_id.to_string(),
+            to: Self::convert_to_meta_user_id(&user_pk),
             amount: points,
-            description: "Reward points for user activity".to_string(),
+            description,
             month,
         };
 
         let res = self.cli.post(&path).json(&body).send().await?;
         Ok(res.json().await?)
     }
+
+    pub async fn get_user_balance(
+        &self,
+        user_pk: Partition,
+        month: String,
+    ) -> Result<UserPointBalanceResponse> {
+        let path = format!(
+            "{}/projects/{}/points/{}??month={}",
+            self.base_url,
+            self.project_id,
+            Self::convert_to_meta_user_id(&user_pk),
+            month
+        );
+
+        let res = self.cli.get(&path).send().await?;
+        Ok(res.json().await?)
+    }
+
+    pub async fn get_user_transactions(
+        &self,
+        user_pk: Partition,
+        month: String,
+        bookmark: Option<String>,
+        limit: Option<i32>,
+    ) -> Result<ListItemsResponse<UserPointTransactionResponse>> {
+        let mut path = format!(
+            "{}/projects/{}/points/{}/transactions?month={}",
+            self.base_url,
+            self.project_id,
+            Self::convert_to_meta_user_id(&user_pk),
+            month
+        );
+        if let Some(bm) = bookmark {
+            path = format!("{}&bookmark={}", path, bm);
+        }
+        if let Some(lim) = limit {
+            path = format!("{}&limit={}", path, lim);
+        }
+
+        let res = self.cli.get(&path).send().await?;
+        let transactions = res.json().await?;
+        Ok(transactions)
+    }
 }
 
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, JsonSchema, OperationIo)]
+
 pub struct AwardPointRequest {
     pub tx_type: String,
     pub to: String,
     pub amount: i64,
     pub description: String,
     pub month: Option<String>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, JsonSchema, OperationIo)]
+
+pub struct AwardPointResponse {
+    pub month: String,
+    pub transaction_id: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, JsonSchema, OperationIo)]
+
+pub struct UserPointTransactionResponse {
+    pub month: String,
+    pub transaction_type: String,
+    pub amount: i64,
+    pub target_user_id: Option<String>,
+    pub description: Option<String>,
+    pub created_at: i64,
+}
+#[derive(Debug, Clone, Serialize, Deserialize, JsonSchema, OperationIo)]
+
+pub struct UserPointBalanceResponse {
+    pub month: String,
+    pub balance: i64,
+    pub total_earned: i64,
+    pub total_spent: i64,
+    pub updated_at: i64,
 }
