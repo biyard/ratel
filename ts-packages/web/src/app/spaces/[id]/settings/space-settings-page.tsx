1892ec57bd265e0748ad0498f49dbf96465e0edf
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2025-12-15 13:22:58 +0900

    fix by card component

--- ts-packages/web/src/app/spaces/[id]/settings/space-settings-page.tsx
+++ ts-packages/web/src/app/spaces/[id]/settings/space-settings-page.tsx
@@ -4,6 +4,7 @@ import { Checkbox } from '@/components/checkbox/checkbox';
 import { useSettingsI18n } from './i18n';
 import SwitchButton from '@/components/switch-button';
 import { SpacePublishState } from '@/features/spaces/types/space-common';
+import Card from '@/components/card';
 
 export function SpaceSettingsPage() {
   const { spacePk } = useParams<{ spacePk: string }>();
@@ -33,23 +34,25 @@ export function SpaceSettingsPage() {
     <div className="flex flex-col gap-6 p-6">
       <div className="text-2xl font-bold text-text-primary">{i18n.title}</div>
 
-      <div className="flex flex-col gap-4">
-        <div className="text-lg font-semibold text-text-primary">
-          {i18n.participation_title}
-        </div>
+      <div className="flex flex-col w-full gap-3">
+        <Card key={'participant-setting'}>
+          <div className="flex flex-col gap-4">
+            <div className="text-sm font-semibold text-text-primary">
+              {i18n.participation_title}
+            </div>
 
-        <Checkbox
-          id="anonymous-participation"
-          value={ctrl.anonymousParticipation}
-          disabled={
-            ctrl.data.space.data.publishState !== SpacePublishState.Draft
-          }
-          onChange={ctrl.handleAnonymousParticipationChange}
-        >
-          <span className="font-medium text-text-primary">
-            {i18n.anonymous_participation_label}
-          </span>
-          {/* <Tooltip>
+            <Checkbox
+              id="anonymous-participation"
+              value={ctrl.anonymousParticipation}
+              disabled={
+                ctrl.data.space.data.publishState !== SpacePublishState.Draft
+              }
+              onChange={ctrl.handleAnonymousParticipationChange}
+            >
+              <span className="text-sm font-medium text-text-primary">
+                {i18n.anonymous_participation_label}
+              </span>
+              {/* <Tooltip>
             <TooltipTrigger asChild>
               <Info />
             </TooltipTrigger>
@@ -57,33 +60,37 @@ export function SpaceSettingsPage() {
               <p>Add to library</p>
             </TooltipContent>
           </Tooltip> */}
-        </Checkbox>
+            </Checkbox>
+          </div>
+        </Card>
+
+        {ctrl.data.space.data.publishState === SpacePublishState.Published && (
+          <Card key={'public-setting'}>
+            <div className="flex flex-col gap-4">
+              <div className="text-sm font-semibold text-text-primary">
+                {i18n.public_setting}
+              </div>
+
+              <div className="flex flex-row w-fit gap-4 items-center">
+                <span className="text-sm/[24px] font-normal text-text-primary">
+                  {value ? i18n.public : i18n.private}
+                </span>
+                <SwitchButton
+                  value={value}
+                  onChange={() => {
+                    if (value) {
+                      ctrl.handleActionPrivate();
+                    } else {
+                      ctrl.handleActionPublic();
+                    }
+                  }}
+                  color="bg-[#fcb300]"
+                />
+              </div>
+            </div>
+          </Card>
+        )}
       </div>
-
-      {ctrl.data.space.data.publishState === SpacePublishState.Published && (
-        <div className="flex flex-col gap-4">
-          <div className="text-lg font-semibold text-text-primary">
-            {i18n.public_setting}
-          </div>
-
-          <div className="flex flex-row w-fit gap-4 items-center">
-            <span className="text-[15px]/[24px] font-normal text-text-primary">
-              {value ? i18n.public : i18n.private}
-            </span>
-            <SwitchButton
-              value={value}
-              onChange={() => {
-                if (value) {
-                  ctrl.handleActionPrivate();
-                } else {
-                  ctrl.handleActionPublic();
-                }
-              }}
-              color="bg-[#fcb300]"
-            />
-          </div>
-        </div>
-      )}
     </div>
   );
 }
