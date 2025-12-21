f3c62dbd4d5d3cfc997b960fae46cbf0b82ee1a1
salman <salman2020@gmail.com>
2025-12-22 00:37:41 +0100

    fix for reviews

--- packages/main-api/src/controllers/v3/spaces/files/mod.rs
+++ packages/main-api/src/controllers/v3/spaces/files/mod.rs
@@ -1,13 +1,9 @@
 pub mod get_files;
 pub mod update_files;
-pub mod link_file;
-pub mod unlink_file;
 pub mod get_file_links;
 
 pub use get_files::*;
 pub use update_files::*;
-pub use link_file::*;
-pub use unlink_file::*;
 pub use get_file_links::*;
 
 #[cfg(test)]
@@ -21,7 +17,6 @@ use by_axum::axum::*;
 pub fn route() -> Router<AppState> {
     Router::new()
         .route("/", patch(update_files_handler).get(get_files_handler))
-        .route("/links", post(link_file_handler).get(list_file_links_handler))
-        .route("/unlink", post(unlink_file_handler))
+        .route("/links", get(list_file_links_handler))
         .route("/by-target", get(get_files_by_target_handler))
 }
