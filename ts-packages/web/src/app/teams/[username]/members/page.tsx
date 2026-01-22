797c4040bcb7841a10a4faa8fcf3eaf3bcb2996f
ryancho-0 <ryan@biyard.co>
2026-01-23 07:48:12 +0900

    refactor(web): change teams page structure

--- ts-packages/web/src/app/teams/[username]/members/page.tsx
+++ ts-packages/web/src/app/teams/[username]/members/page.tsx
@@ -1,7 +1,11 @@
+import MembersPage from '@/features/teams/members/pages/members-page';
 import { useParams } from 'react-router';
-import MembersPage from './members-page';
 
 export default function Page() {
   const { username } = useParams<{ username: string }>();
+
+  if (!username) {
+    return <div>Username not found</div>;
+  }
   return <MembersPage username={username!} />;
 }
