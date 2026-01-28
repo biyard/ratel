bb68b92d8330bb9c50ac569c925f0637ac9880fa
ryancho-0 <ryan@biyard.co>
2026-01-28 15:16:09 +0900

    feat(main-api): add admin team migration handler

--- packages/main-api/src/controllers/v3/teams/find_team.rs
+++ packages/main-api/src/controllers/v3/teams/find_team.rs
@@ -26,13 +26,12 @@ pub async fn find_team_handler(
     NoApi(user): NoApi<Option<User>>,
     Query(params): Query<FindTeamQueryParams>,
 ) -> Result<Json<TeamResponse>, Error> {
-    let team_query_option = Team::opt().sk("TEAM#".to_string());
+    let team_query_option = Team::opt_one();
 
     let (team, _) =
         Team::find_by_username_prefix(&dynamo.client, params.username.clone(), team_query_option)
             .await?;
 
-    tracing::debug!("find_team: items={:?}", team.clone());
     let team = team.into_iter().next().ok_or(Error::TeamNotFound)?;
 
     let permissions = if let Some(user) = user {
