2f29e27e5fcf3e605c21bc892403782e57cfc9c5
jaebinGit <tnwoql327@gmail.com>
2026-02-10 20:03:37 +0900

    feat: add DashboardExtension types for dynamic widgets

--- packages/space-page-dashboard/src/lib.rs
+++ packages/space-page-dashboard/src/lib.rs
@@ -1,12 +1,15 @@
 mod assets;
+pub mod components;
 mod menu;
 pub mod route;
-mod views;
+pub mod types;
+pub mod views;
 
-pub use assets::*;
+pub use types::*;
 use dioxus::prelude::*;
 
 pub use menu::get_nav_item;
 pub use route::Route;
 
 use common::*;
+use components::*;
