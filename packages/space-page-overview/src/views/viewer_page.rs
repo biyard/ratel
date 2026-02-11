7c336997677e18fd1608ec3f9249621abf0b7dba
ryancho-0 <ryan@biyard.co>
2026-02-11 20:37:08 +0900

    feat(dioxus-components): add tiptap-editor

--- packages/space-page-overview/src/views/viewer_page.rs
+++ packages/space-page-overview/src/views/viewer_page.rs
@@ -1,8 +1,19 @@
 use super::*;
+use common::components::TiptapEditor;
 
 #[component]
 pub fn ViewerPage(space_id: SpacePartition) -> Element {
+    let mut content = use_signal(|| "<p>TESTESTES<span>TSETSE</span></p>".to_string());
+
     rsx! {
-        "Viewer page"
+        TiptapEditor {
+            class: "w-full h-200",
+            content: content(),
+            editable: true,
+            placeholder: "Type here...",
+            on_content_change: move |html: String| {
+                content.set(html);
+            },
+        }
     }
 }
