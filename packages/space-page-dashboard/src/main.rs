997318850f31752dc8e02fa7e470d7aeb7647e3f
jaebinGit <tnwoql327@gmail.com>
2026-02-13 11:08:48 +0900

    chore: add style link in space-page-dashboard

--- packages/space-page-dashboard/src/main.rs
+++ packages/space-page-dashboard/src/main.rs
@@ -12,6 +12,9 @@ pub const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 #[component]
 fn App() -> Element {
     rsx! {
+        document::Link { rel: "icon", href: FAVICON }
+        document::Link { rel: "stylesheet", href: MAIN_CSS }
+        document::Link { rel: "stylesheet", href: TAILWIND_CSS }
         Router::<Route> {}
     }
 }
