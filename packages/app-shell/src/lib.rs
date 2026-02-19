2dfaaa3b2f1839b76be18e32e42012e79eebe0f6
hackartists <hackartists@gmail.com>
2026-02-19 13:16:42 +0900

    Implement app shell

---
+++ packages/app-shell/src/lib.rs
@@ -0,0 +1,19 @@
+#![allow(unused_imports, dead_code)]
+pub mod config;
+mod constants;
+// pub mod interop;
+mod route;
+pub mod views;
+
+pub use route::Route;
+
+use common::*;
+use dioxus::prelude::*;
+
+type Result<T> = common::Result<T>;
+
+#[cfg(not(feature = "server"))]
+pub mod web;
+
+#[cfg(feature = "server")]
+pub mod server;
