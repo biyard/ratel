62e3cfe54f630f8031c0dce0b86896784a7537db
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2026-02-20 15:28:21 +0900

    fix route bugfix

--- packages/ratel-user-shell/src/layout.rs
+++ packages/ratel-user-shell/src/layout.rs
@@ -8,3 +8,13 @@ pub fn AppLayout() -> Element {
         }
     }
 }
+
+#[component]
+pub fn UserLayout(username: String) -> Element {
+    let _ = username;
+    rsx! {
+        div { class: "flex flex-col w-full min-h-screen bg-space-bg text-font-primary",
+            div { class: "flex flex-col grow p-5", Outlet::<Route> {} }
+        }
+    }
+}
