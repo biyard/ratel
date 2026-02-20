c9ffbf98a113edae99eea98ba6e0507dc3f2297d
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2026-02-20 14:19:24 +0900

    add ratel user mock package

--- packages/ratel-user-home/src/route.rs
+++
@@ -1,9 +0,0 @@
-use crate::*;
-
-use views::Home;
-#[derive(Debug, Clone, Routable, PartialEq)]
-#[rustfmt::skip]
-pub enum Route {
-    #[route("/:username")]
-    Home { username: String },
-}
---
+++ packages/ratel-user-setting/src/views/home/mod.rs
@@ -0,0 +1,15 @@
+mod some_component;
+use some_component::SomeComponent;
+
+use dioxus::prelude::*;
+
+#[component]
+pub fn Home() -> Element {
+    rsx! {
+        div { class: "flex flex-col items-center justify-center w-full h-full",
+            h1 { class: "text-2xl font-bold", "ratel-user-setting" }
+            p { class: "mt-2 text-gray-500", "Coming soon..." }
+            SomeComponent {}
+        }
+    }
+}
