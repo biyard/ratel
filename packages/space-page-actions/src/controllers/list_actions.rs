daa992d151a769be360ec22d248723b93af8291c
ryancho-0 <ryan@biyard.co>
2026-02-20 14:36:08 +0900

    fix(space-action-poll):  update list action

--- packages/space-page-actions/src/controllers/list_actions.rs
+++ packages/space-page-actions/src/controllers/list_actions.rs
@@ -9,23 +9,25 @@ pub async fn list_actions(
     // bookmark: Option<String>,
 ) -> Result<Vec<SpaceAction>> {
     let cli = crate::config::get().common.dynamodb();
-
-    let poll_future = SpacePoll::query(cli, space_pk.clone(), SpacePoll::opt_all());
+    let space_pk: Partition = space_pk.into();
+    let poll_future = SpacePoll::query(
+        cli,
+        space_pk.clone(),
+        SpacePoll::opt_all().sk(EntityType::SpacePoll(String::default()).to_string()),
+    );
     let ((polls, _),) = tokio::try_join!(poll_future)?;
-
     let actions = if let Some(user) = user.0 {
-        let space_pk_partition: Partition = space_pk.into();
-
         let keys: Vec<_> = polls
             .iter()
-            .map(|poll| SpacePollUserAnswer::keys(&user.pk, &poll.sk, &space_pk_partition))
+            .map(|poll| SpacePollUserAnswer::keys(&user.pk, &poll.sk, &space_pk))
             .collect();
         let (user_participated,) = tokio::try_join!(SpacePollUserAnswer::batch_get(cli, keys))?;
-
         let participated_poll_sks: HashSet<String> = user_participated
             .into_iter()
             .filter_map(|a| match a.sk {
-                EntityType::SpacePollUserAnswer(_, poll_sk) => Some(poll_sk),
+                EntityType::SpacePollUserAnswer(_, poll_sk) => {
+                    Some(SpacePollUserAnswer::parse_wrong_sk(poll_sk))
+                }
                 _ => None,
             })
             .collect();
@@ -40,6 +42,6 @@ pub async fn list_actions(
     } else {
         polls.into_iter().map(|poll| (poll, false).into()).collect()
     };
-
+    debug!("actions: {:?}", actions);
     Ok(actions)
 }
--- packages/space-page-actions/src/views/mod.rs
+++ packages/space-page-actions/src/views/mod.rs
@@ -1,15 +1,16 @@
-use crate::*;
+use crate::{controllers::list_actions, *};
 mod i18n;
 use i18n::ListActionPageTranslate;
 #[component]
 pub fn ListActionPage(space_id: SpacePartition) -> Element {
     let tr: ListActionPageTranslate = use_translate();
-
+    let actions = use_loader(move || list_actions(space_id.clone()))?;
     rsx! {
         div {
             id: "new-action-page",
             class: "flex flex-col gap-5 items-start w-full",
             h3 { "{tr.title}" }
+            h3 { "{actions.len()}" }
         }
     }
 }
