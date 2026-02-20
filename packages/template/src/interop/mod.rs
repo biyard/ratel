a6ede066839913d155d7524b630a929cb631802a
ryancho-0 <ryan@biyard.co>
2026-02-20 09:25:55 +0900

    fix(template): add wasm import

--- packages/template/src/interop/mod.rs
+++ packages/template/src/interop/mod.rs
@@ -1,5 +1,8 @@
 use common::wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::js_sys::Promise;
 
+use crate::*;
 #[wasm_bindgen(js_namespace = ["window", "ratel", "{{crate_name}}"])]
 extern "C" {
     #[wasm_bindgen(js_name = initialize)]
