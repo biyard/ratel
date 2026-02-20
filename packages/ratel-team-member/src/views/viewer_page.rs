3e93bd2aafebeeed542544e7bdcb7ae14f81390c
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2026-02-20 17:03:51 +0900

    implement admin, user mock ui

--- packages/ratel-team-member/src/views/viewer_page.rs
+++ packages/ratel-team-member/src/views/viewer_page.rs
@@ -1,16 +1,12 @@
-mod some_component;
-use some_component::SomeComponent;
-
 use dioxus::prelude::*;
 
 #[component]
-pub fn Home(teamname: String) -> Element {
-    debug!("Team Member teamname: {}", teamname);
+pub fn ViewerPage(teamname: String) -> Element {
     rsx! {
         div { class: "flex flex-col items-center justify-center w-full h-full",
             h1 { class: "text-2xl font-bold", "ratel-team-member" }
             p { class: "mt-2 text-gray-500", "Coming soon..." }
-            SomeComponent {}
+            p { class: "mt-2 text-gray-400", "team: {teamname}" }
         }
     }
 }
