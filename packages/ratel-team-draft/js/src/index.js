ecea5267c65683ec22fbc277dc700bad8499c993
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2026-02-20 15:31:37 +0900

    make team package

---
+++ packages/ratel-team-draft/js/src/index.js
@@ -0,0 +1,11 @@
+if (typeof window !== "undefined") {
+  if (typeof window.ratel === "undefined") {
+    window.ratel = {};
+  }
+
+  window.ratel.ratel_team_draft = {
+    initialize: (_conf) => {
+      console.debug("Initializing ratel_team_draft with config");
+    },
+  };
+}
