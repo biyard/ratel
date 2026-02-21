43f31ffd30dc52ab77dfe4f2f4810e467b8da269
ryancho-0 <ryan@biyard.co>
2026-02-21 16:26:04 +0900

    fix: build error

--- app/spaces/pages/actions/src/route.rs
+++ app/spaces/pages/actions/src/route.rs
@@ -1,8 +1,6 @@
 use crate::*;
 
 use crate::views::MainPage;
-use dioxus::router::components::child_router::ChildRouter;
-use dioxus::router::RouterContext;
 use space_action_poll::Route as PollRoute;
 
 #[derive(Debug, Clone, Routable, PartialEq)]
@@ -19,6 +17,6 @@ pub enum Route {
 #[component]
 fn PollApp(space_id: SpacePartition, rest: Vec<String>) -> Element {
     rsx! {
-        Route::<PollRoute> {}
+        Router::<PollRoute> {}
     }
 }
