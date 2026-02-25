e230af914b8d8aeb042917c2df942fb33d1afeaa
hackartists <hackartists@gmail.com>
2026-02-25 15:37:54 +0900

    feat(app-shell): add AWS Lambda support for server deployment
    - Introduced `lambda_http` dependency for Lambda integration.
    - Added a new `lambda` feature in `Cargo.toml` to enable Lambda-specific functionality.
    - Updated `Makefile` with `build-lambda` target for Lambda builds.
    - Enhanced `serve` function in `mod.rs` to support both standard and Lambda environments.
    - Modified GitHub Actions workflow to align with the new Lambda deployment process.

--- app/shell/src/server/mod.rs
+++ app/shell/src/server/mod.rs
@@ -1,17 +1,32 @@
 use crate::*;
 
-pub fn serve(app: fn() -> Element) {
+pub async fn serve(app: fn() -> Element) {
     let config = config::get();
+    let cli = config.common.dynamodb();
+    let session_layer =
+        common::middlewares::session_layer::get_session_layer(cli, config.common.env.to_string());
+    let dioxus_router = dioxus::server::router(app);
+    let app = dioxus_router.layer(session_layer);
 
+    #[cfg(not(feature = "lambda"))]
     dioxus::serve(move || {
-        let cli = config.common.dynamodb();
-        let session_layer = common::middlewares::session_layer::get_session_layer(
-            cli,
-            config.common.env.to_string(),
-        );
-        async move {
-            let dioxus_router = dioxus::server::router(app);
-            Ok(dioxus_router.layer(session_layer))
-        }
+        let app = app.clone();
+        async move { Ok(app) }
     });
+
+    #[cfg(feature = "lambda")]
+    {
+        use lambda_http::run;
+        let app_future = async move { run(app).await };
+
+        if let Ok(handle) = tokio::runtime::Handle::try_current() {
+            handle.block_on(app_future);
+        } else {
+            tokio::runtime::Builder::new_multi_thread()
+                .enable_all()
+                .build()
+                .unwrap()
+                .block_on(app_future);
+        }
+    }
 }
