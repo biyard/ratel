1ece77d3e4165236dbe874c845448e6bf27d53fc
hackartists <hackartists@gmail.com>
2026-02-25 17:51:36 +0900

    feat(shell): enhance build process and server initialization
    - Updated Makefile to improve build commands with debug-symbols flag
    - Added debug logging for configuration in `main.rs`
    - Refactored server initialization to streamline middleware setup
    - Removed unused profile configurations from root `Cargo.toml`

--- app/shell/src/server/mod.rs
+++ app/shell/src/server/mod.rs
@@ -1,17 +1,22 @@
 use crate::*;
 
-pub async fn serve(app: fn() -> Element) {
+pub fn serve(app: fn() -> Element) {
     let config = config::get();
-    let cli = config.common.dynamodb();
-    let session_layer =
-        common::middlewares::session_layer::get_session_layer(cli, config.common.env.to_string());
-    let dioxus_router = dioxus::server::router(app);
-    let app = dioxus_router.layer(session_layer);
 
     #[cfg(not(feature = "lambda"))]
     dioxus::serve(move || {
-        let app = app.clone();
-        async move { Ok(app) }
+        let cli = config.common.dynamodb();
+        let session_layer = common::middlewares::session_layer::get_session_layer(
+            cli,
+            config.common.env.to_string(),
+        );
+
+        async move {
+            let dioxus_router = dioxus::server::router(app);
+            let app = dioxus_router.layer(session_layer);
+
+            Ok(app)
+        }
     });
 
     #[cfg(feature = "lambda")]
@@ -19,10 +24,11 @@ pub async fn serve(app: fn() -> Element) {
         use lambda_http::run;
         let app_future = async move { run(app).await };
 
+        info!("Starting server in Lambda environment");
         if let Ok(handle) = tokio::runtime::Handle::try_current() {
-            handle.block_on(app_future);
+            let _ = handle.block_on(app_future);
         } else {
-            tokio::runtime::Builder::new_multi_thread()
+            let _ = tokio::runtime::Builder::new_multi_thread()
                 .enable_all()
                 .build()
                 .unwrap()
