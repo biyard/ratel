044e3fbb4474ef73d18c7ba768b1652ed677634d
jaebinGit <tnwoql327@gmail.com>
2026-02-27 19:53:04 +0900

    feat(incentive-pool): add space incentive models and server endpoints

---
+++ app/spaces/apps/incentive-pool/src/controllers/mod.rs
@@ -0,0 +1,9 @@
+mod create_space_incentive;
+mod get_space_incentive;
+mod list_space_incentive_tokens;
+mod refresh_space_incentive_tokens;
+
+pub use create_space_incentive::*;
+pub use get_space_incentive::*;
+pub use list_space_incentive_tokens::*;
+pub use refresh_space_incentive_tokens::*;
--- app/spaces/apps/incentive-pool/src/lib.rs
+++ app/spaces/apps/incentive-pool/src/lib.rs
@@ -1,9 +1,17 @@
+mod components;
+mod controllers;
 mod i18n;
+mod interop;
+mod models;
 mod route;
+mod utils;
 mod views;
 
 use dioxus::prelude::*;
 
+pub use controllers::*;
 pub use route::Route;
 
 use common::*;
+
+type Result<T> = common::Result<T>;
