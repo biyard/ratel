3b1f1c05674453faf3ad18fc3cfaeecd70412ad8
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2026-03-03 17:56:57 +0900

    connect like api

--- app/posts/src/components/feed_list/mod.rs
+++ app/posts/src/components/feed_list/mod.rs
@@ -1,5 +1,6 @@
 use crate::components::FeedCard;
 use crate::controllers::dto::*;
+use crate::controllers::like_post::like_post_handler;
 use crate::controllers::list_posts::list_posts_handler;
 use crate::*;
 use common::hooks::use_infinite_query;
@@ -13,7 +14,16 @@ pub fn FeedList() -> Element {
         div { class: "flex flex-col flex-1 max-mobile:px-[10px]",
             div { class: "flex flex-col flex-1 gap-2.5",
                 for post in v.items() {
-                    FeedCard { key: "post-{post.pk}", post: post.clone() }
+                    FeedCard {
+                        key: "post-{post.pk}",
+                        post: post.clone(),
+                        on_like: move |value| {
+                            let post_pk = post.pk.clone();
+                            spawn(async move {
+                                let _ = like_post_handler(post_pk, value).await;
+                            });
+                        },
+                    }
                 }
 
                 if v.has_more() {
