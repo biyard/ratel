19e619a1866da9624acbf23509b976dc2d30a4b1
ryancho-0 <ryan@biyard.co>
2026-03-06 07:33:08 +0900

    fix(build): error

--- app/spaces/pages/dashboard/src/controllers/list_ranking.rs
+++ app/spaces/pages/dashboard/src/controllers/list_ranking.rs
@@ -3,8 +3,8 @@ use crate::*;
 
 #[get("/api/spaces/:space_id/incentive/ranking?bookmark", _space: common::models::space::SpaceCommon)]
 pub async fn list_ranking_handler(
-    space_id: SpacePartition,
-    bookmark: Option<String>,
+    _space_id: SpacePartition,
+    _bookmark: Option<String>,
 ) -> common::Result<space_common::types::dashboard::RankingTableData> {
     use space_common::types::dashboard::*;
 
