4bd95a7c32fa2f76dfb0588bb1bc10f51ff7e635
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2026-03-05 19:24:03 +0900

    implement my network page

--- app/my-follower/src/controllers/follow_user.rs
+++ app/my-follower/src/controllers/follow_user.rs
@@ -2,60 +2,55 @@ use crate::*;
 use common::models::auth::UserFollow;
 use ratel_post::models::Team;
 
-#[delete(
-    "/api/spaces/{space_id}/subscriptions/users/subscribe",
-    role: SpaceUserRole,
-    user: ratel_auth::User
-)]
-pub async fn unsubscribe_user(space_id: SpacePartition, user_pk: Partition) -> Result<()> {
-    let _space_id = space_id;
+#[post("/api/my-follower/follow", user: ratel_auth::User)]
+pub async fn follow_user(target_pk: Partition) -> Result<()> {
     let common_config = common::CommonConfig::default();
     let cli = common_config.dynamodb();
 
-    let target_pk = user_pk.clone();
     if target_pk == user.pk {
-        return Err(Error::BadRequest("Cannot unsubscribe yourself".into()));
+        return Err(Error::BadRequest("Cannot follow yourself".into()));
     }
 
     let (follower_pk, follower_sk) = UserFollow::follower_keys(&target_pk, &user.pk);
     if UserFollow::get(cli, follower_pk.clone(), Some(follower_sk.clone()))
         .await?
-        .is_none()
+        .is_some()
     {
         return Ok(());
     }
 
-    let (following_pk, following_sk) = UserFollow::following_keys(&user.pk, &target_pk);
+    let (follower_record, following_record) =
+        UserFollow::new_follow_records(user.pk.clone(), target_pk.clone());
 
-    let follower_delete = UserFollow::delete_transact_write_item(&follower_pk, &follower_sk);
-    let following_delete = UserFollow::delete_transact_write_item(&following_pk, &following_sk);
+    let follow_tx = follower_record.create_transact_write_item();
+    let following_tx = following_record.create_transact_write_item();
     let target_update = match &target_pk {
         Partition::User(_) => {
             common::models::auth::User::updater(target_pk.clone(), EntityType::User)
-                .decrease_followers_count(1)
+                .increase_followers_count(1)
                 .transact_write_item()
         }
         Partition::Team(_) => Team::updater(target_pk.clone(), EntityType::Team)
-            .decrease_followers(1)
+            .increase_followers(1)
             .transact_write_item(),
         _ => return Err(Error::BadRequest("Invalid target".into())),
     };
     let follower_update = common::models::auth::User::updater(user.pk.clone(), EntityType::User)
-        .decrease_followings_count(1)
+        .increase_followings_count(1)
         .transact_write_item();
 
     cli.transact_write_items()
         .set_transact_items(Some(vec![
-            follower_delete,
-            following_delete,
+            follow_tx,
+            following_tx,
             target_update,
             follower_update,
         ]))
         .send()
         .await
         .map_err(|e| {
-            error!("Failed to unsubscribe user: {:?}", e);
-            Error::Unknown("Failed to unsubscribe user".into())
+            error!("Failed to follow user: {:?}", e);
+            Error::Unknown("Failed to follow user".into())
         })?;
 
     Ok(())
---
+++ app/my-follower/src/views/home/i18n.rs
@@ -0,0 +1,14 @@
+use crate::*;
+
+translate! {
+    MyFollowerTranslate;
+
+    follow_toast: {
+        en: "Followed.",
+        ko: "팔로우되었습니다."
+    }
+    unfollow_toast: {
+        en: "Unfollowed.",
+        ko: "언팔로우되었습니다."
+    }
+}
