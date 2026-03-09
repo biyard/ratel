9116087afec5f23019ae43cda40db8a5d722369d
hackartists <hackartists@gmail.com>
2026-03-09 20:39:43 +0900

    refactor(shell): replace `ratel-post` with internal `posts` module
    - Removed `ratel-post` dependency from `Cargo.toml` and feature groups.
    - Added a new `posts` module under `features`.
    - Updated `route.rs` to use the internal `posts` module instead of `ratel-post`.

--- app/shell/src/route.rs
+++ app/shell/src/route.rs
@@ -11,7 +11,7 @@ use membership::Home as MembershipHome;
 use crate::features::admin::Route as AdminRoute;
 use ratel_auth::Route as AuthRoute;
 use crate::features::my_follower::Route as MyFollowerRoute;
-use ratel_post::Route as PostRoute;
+use crate::features::posts::Route as PostRoute;
 use space_shell::Route as SpaceRoute;
 
 #[derive(Debug, Clone, Routable, PartialEq)]
