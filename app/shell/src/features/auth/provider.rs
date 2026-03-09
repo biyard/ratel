6163bb14caebfef6299586d36ba11e602785248a
hackartists <hackartists@gmail.com>
2026-03-10 01:44:07 +0900

    refactor(auth): comment out unused imports and initialization code
    - Commented out `initialize` and related Firebase configuration code.
    - Added a constant `MAIN_JS` for the asset path.
    - These changes clean up unused code and improve maintainability.

--- app/shell/src/features/auth/provider.rs
+++ app/shell/src/features/auth/provider.rs
@@ -1,13 +1,15 @@
 use crate::features::auth::*;
 
+pub const MAIN_JS: Asset = asset!("/assets/ratel-app-shell.js");
+
 #[component]
 pub fn Provider() -> Element {
     #[cfg(feature = "web")]
     {
-        use crate::features::auth::interop::initialize;
+        // use crate::features::auth::interop::initialize;
 
-        let fb_conf = crate::common::FirebaseConfig::default().into();
-        initialize(&fb_conf);
+        // let fb_conf = crate::common::FirebaseConfig::default().into();
+        // initialize(&fb_conf);
 
         use crate::features::auth::interop::wallet_connect_initialize;
         let wc_conf = crate::common::WalletConnectConfig::default();
