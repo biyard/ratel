554522eebf5ba3c27bf0dfae306277de33eb2ad0
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2026-03-10 10:18:50 +0900

    implement mock participant card

--- app/ratel/src/features/spaces/space_common/components/space_nav/mod.rs
+++ app/ratel/src/features/spaces/space_common/components/space_nav/mod.rs
@@ -1,23 +1,36 @@
 use crate::features::spaces::space_common::*;
+mod participation_card;
 mod space_user_login;
 mod space_user_profile;
 
 use crate::common::models::User;
+pub use participation_card::*;
 pub use space_user_login::*;
 pub use space_user_profile::*;
 
 #[component]
 pub fn SpaceNav(
+    space_id: SpacePartition,
     logo: String,
     menus: Vec<SpaceNavItem>,
     user: Option<User>,
     login_handler: EventHandler<()>,
     role: SpaceUserRole,
+    show_participation_card: bool,
+    credential_path: Option<String>,
 ) -> Element {
     rsx! {
         div { class: "flex z-40 flex-col gap-2.5 justify-between pt-2.5 h-full divide-y shrink-0 divide-divider w-full",
             div { class: "flex flex-col gap-2.5 w-full pb-4",
                 img { src: "{logo}", class: "mx-4 mt-5 mb-2.5 w-25" }
+
+                if show_participation_card {
+                    ParticipationCard {
+                        space_id,
+                        credential_path,
+                        on_login: login_handler,
+                    }
+                }
 
                 div { class: "flex flex-col gap-1.5 items-start px-4 pt-2.5 font-bold text-xs/[14px]",
                     for item in menus.iter() {
