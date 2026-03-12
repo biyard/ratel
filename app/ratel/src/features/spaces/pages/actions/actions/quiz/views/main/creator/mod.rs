c2914d3fc8be24339410a95816a9ea96f16fe20f
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2026-03-12 11:32:09 +0900

    implement overview page

--- app/ratel/src/features/spaces/pages/actions/actions/quiz/views/main/creator/mod.rs
+++ app/ratel/src/features/spaces/pages/actions/actions/quiz/views/main/creator/mod.rs
@@ -1,17 +1,16 @@
 use crate::features::spaces::pages::actions::actions::quiz::components::*;
-use crate::features::spaces::pages::actions::actions::quiz::controllers::*;
 use crate::features::spaces::pages::actions::actions::quiz::*;
+use crate::features::spaces::pages::actions::ActionCommonSettings;
 mod i18n;
-mod overview_page;
-mod quiz_page;
-mod setting_page;
-mod upload_page;
-use crate::features::spaces::space_common::types::space_page_actions_quiz_key;
-use i18n::QuizCreatorTranslate;
-pub use overview_page::OverviewPage;
-pub use quiz_page::*;
-pub use setting_page::*;
-pub use upload_page::*;
+mod overview_tab;
+mod quiz_tab;
+mod setting_tab;
+mod upload_tab;
+pub use i18n::QuizCreatorTranslate;
+pub use overview_tab::*;
+pub use quiz_tab::*;
+pub use setting_tab::*;
+pub use upload_tab::*;
 
 #[derive(Clone, Copy, PartialEq, Eq)]
 pub(crate) enum QuizCreatorSection {
@@ -22,134 +21,38 @@ pub(crate) enum QuizCreatorSection {
 }
 
 #[component]
-pub fn QuizCreatorPage(space_id: SpacePartition, quiz_id: SpaceQuizEntityType) -> Element {
+pub fn QuizCreatorPage(
+    space_id: ReadSignal<SpacePartition>,
+    quiz_id: ReadSignal<SpaceQuizEntityType>,
+) -> Element {
     let tr: QuizCreatorTranslate = use_translate();
-    let space_id = use_signal(|| space_id);
-    let quiz_id = use_signal(|| quiz_id);
-    let key = space_page_actions_quiz_key(&space_id(), &quiz_id());
-    let quiz_loader = use_query(&key, { move || get_quiz(space_id(), quiz_id()) })?;
-    let answer_key = {
-        let mut k = key.clone();
-        k.push("answers".into());
-        k
-    };
-    let answer_loader = use_query(&answer_key, {
-        move || get_quiz_answer(space_id(), quiz_id())
-    })?;
-
-    let quiz = quiz_loader.read().clone();
-    let answer = answer_loader.read().clone();
-
-    let mut editing = use_signal(|| false);
-    let mut original_questions = use_signal(|| quiz.questions.clone());
-    let mut original_answers = use_signal(|| align_answers(&quiz.questions, &answer.answers));
-    let mut questions = use_signal(|| quiz.questions.clone());
-    let mut answers = use_signal(|| align_answers(&quiz.questions, &answer.answers));
-    let mut started_at = use_signal(|| quiz.started_at);
-    let mut ended_at = use_signal(|| quiz.ended_at);
-    let mut retry_count = use_signal(|| quiz.retry_count);
-    let mut pass_score = use_signal(|| quiz.pass_score);
-    let mut current_section = use_signal(|| QuizCreatorSection::Overview);
-
-    let can_edit = quiz.user_response_count == 0;
-    let show_editor = can_edit && editing();
+    let ctx = Context::init(space_id, quiz_id)?;
+    let can_edit = ctx.quiz.read().user_response_count == 0;
 
     rsx! {
-        div { class: "flex w-full flex-col gap-6",
-            // The previous single-page creator flow is kept through the same handlers,
-            // but rendered as section pages for the tab-based editor.
-            match current_section() {
-                QuizCreatorSection::Overview => rsx! {
-                    OverviewPage {
-                        space_id,
-                        quiz_id,
-                        initial_title: quiz.title.clone(),
-                        initial_description: quiz.description.clone(),
-                        can_edit,
-                        current_section,
-                    }
-                },
-                QuizCreatorSection::Upload => rsx! {
-                    UploadPage {
-                        space_id,
-                        quiz_id,
-                        initial_files: quiz.files.clone(),
-                        can_edit,
-                        current_section,
-                    }
-                },
-                QuizCreatorSection::Quiz => rsx! {
-                    QuizPage {
-                        space_id,
-                        quiz_id,
-                        can_edit,
-                        editing,
-                        questions,
-                        answers,
-                        original_questions,
-                        original_answers,
-                        current_section,
-                    }
-                },
-                QuizCreatorSection::Setting => rsx! {
-                    SettingPage {
-                        space_id,
-                        quiz_id,
-                        can_edit,
-                        started_at,
-                        ended_at,
-                        retry_count,
-                        pass_score,
-                        current_section,
-                    }
-                },
+        div { class: "flex min-h-0 w-full flex-1 flex-col gap-4",
+            h3 { {tr.page_title} }
+            Tabs { class: "min-h-0 flex-1", default_value: "overview-tab",
+                TabList {
+                    TabTrigger { index: 0usize, value: "overview-tab", {tr.overview_title} }
+                    TabTrigger { index: 1usize, value: "upload-tab", {tr.upload_title} }
+                    TabTrigger { index: 2usize, value: "quiz-tab", {tr.quiz_section_title} }
+                    TabTrigger { index: 3usize, value: "setting-tab", {tr.setting_section_title} }
+                }
+                TabContent {
+                    index: 0usize,
+                    value: "overview-tab",
+                    class: "flex min-h-0 flex-1",
+                    OverviewTab { can_edit }
+                }
+                TabContent { index: 1usize, value: "upload-tab",
+                    UploadTab { can_edit }
+                }
+                TabContent { index: 2usize, value: "quiz-tab",
+                    QuizTab { can_edit }
+                }
+                TabContent { index: 3usize, value: "setting-tab", ActionCommonSettings {} }
             }
         }
-    }
-}
-
-pub(super) fn align_answers(
-    questions: &[Question],
-    answers: &[QuizCorrectAnswer],
-) -> Vec<QuizCorrectAnswer> {
-    let mut next = Vec::with_capacity(questions.len());
-    for (idx, question) in questions.iter().enumerate() {
-        let answer = answers
-            .get(idx)
-            .cloned()
-            .unwrap_or_else(|| QuizCorrectAnswer::for_question(question));
-        let aligned = match (question, answer) {
-            (Question::MultipleChoice(_), QuizCorrectAnswer::Multiple { answers }) => {
-                QuizCorrectAnswer::Multiple { answers }
-            }
-            (Question::SingleChoice(_), QuizCorrectAnswer::Single { answer }) => {
-                QuizCorrectAnswer::Single { answer }
-            }
-            (Question::MultipleChoice(_), _) => QuizCorrectAnswer::Multiple { answers: vec![] },
-            _ => QuizCorrectAnswer::Single { answer: None },
-        };
-        next.push(aligned);
-    }
-    next
-}
-
-pub(super) fn quiz_answer_to_viewer(
-    question: &Question,
-    answer: &QuizCorrectAnswer,
-) -> Option<Answer> {
-    match (question, answer) {
-        (Question::SingleChoice(_), QuizCorrectAnswer::Single { answer }) => {
-            Some(Answer::SingleChoice {
-                answer: *answer,
-                other: None,
-            })
-        }
-        (Question::MultipleChoice(_), QuizCorrectAnswer::Multiple { answers }) => {
-            Some(Answer::MultipleChoice {
-                answer: Some(answers.clone()),
-                other: None,
-            })
-        }
-        _ => None,
     }
 }
