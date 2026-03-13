eb9baf3a4421ae3e1a99ab00eb1a5adc392f6e48
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2026-03-13 10:24:35 +0900

    fix dx lint bugfix

--- app/ratel/src/features/spaces/pages/actions/actions/discussion/views/main/mod.rs
+++ app/ratel/src/features/spaces/pages/actions/actions/discussion/views/main/mod.rs
@@ -16,34 +16,30 @@ pub fn DiscussionActionPage(
     discussion_id: SpacePostEntityType,
 ) -> Element {
     let role = use_space_role()();
+    let key = space_page_actions_discussion_key(&space_id, &discussion_id);
+    let discussion_loader = use_query(&key, {
+        let space_id = space_id.clone();
+        let discussion_id = discussion_id.clone();
+        move || get_discussion(space_id.clone(), discussion_id.clone())
+    })?;
+
+    let discussion = discussion_loader.read().clone();
+
+    let comments_key = space_page_actions_discussion_comments_key(&space_id, &discussion_id);
+    let comments_loader = use_query(&comments_key, {
+        let space_id = space_id.clone();
+        let discussion_id = discussion_id.clone();
+        move || list_comments(space_id.clone(), discussion_id.clone(), None)
+    })?;
+
+    let comments = comments_loader.read().clone();
 
     match role {
         SpaceUserRole::Creator => rsx! {
             CreatorMain { space_id, discussion_id }
         },
         _ => {
-            let key = space_page_actions_discussion_key(&space_id, &discussion_id);
-            let discussion_loader = use_query(&key, {
-                let space_id = space_id.clone();
-                let discussion_id = discussion_id.clone();
-                move || get_discussion(space_id.clone(), discussion_id.clone())
-            })?;
-
-            let discussion = discussion_loader.read().clone();
-
-            let can_comment =
-                matches!(role, SpaceUserRole::Creator | SpaceUserRole::Participant);
-
-            //FIXME: use InfiniteQuery
-            let comments_key =
-                space_page_actions_discussion_comments_key(&space_id, &discussion_id);
-            let comments_loader = use_query(&comments_key, {
-                let space_id = space_id.clone();
-                let discussion_id = discussion_id.clone();
-                move || list_comments(space_id.clone(), discussion_id.clone(), None)
-            })?;
-
-            let comments = comments_loader.read().clone();
+            let can_comment = matches!(role, SpaceUserRole::Creator | SpaceUserRole::Participant);
 
             rsx! {
                 ViewerMain {
