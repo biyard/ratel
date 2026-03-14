09580a91e9fa45aa21727e0298ab89150447b84d
hackartist <hackartists@gmail.com>
2026-03-14 20:56:09 +0900

    ecs-cluster-stack.ts 업데이트

--- cdk/lib/ecs-cluster-stack.ts
+++ cdk/lib/ecs-cluster-stack.ts
@@ -23,11 +23,6 @@ export class EcsClusterStack extends Stack {
     this.vpc = ec2.Vpc.fromLookup(this, "Vpc", { isDefault: true });
     this.cluster = new ecs.Cluster(this, "Cluster", { vpc: this.vpc });
 
-    this.namespace = new sd.PrivateDnsNamespace(this, "Namespace", {
-      name: `ratel-svc.local`,
-      vpc: this.vpc,
-    });
-
     this.namespace = new sd.PrivateDnsNamespace(this, `${props.stage}-Namespace`, {
       name: `ratel-${props.stage}-svc.local`,
       vpc: this.vpc,
