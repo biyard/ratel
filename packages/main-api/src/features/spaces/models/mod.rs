867cb9be7b173c92acdeefe237dd87b4adcaca1f
ryancho-0 <ryan@biyard.co>
2026-03-26 17:49:23 +0900

    feat(settings): add space admin

--- packages/main-api/src/features/spaces/models/mod.rs
+++ packages/main-api/src/features/spaces/models/mod.rs
@@ -1,8 +1,10 @@
+pub mod space_admin;
 pub mod space_common;
 pub mod space_participant;
 mod space_requirement;
 pub mod space_report;
 
+pub use space_admin::*;
 pub use space_common::*;
 pub use space_participant::*;
 pub use space_requirement::*;
