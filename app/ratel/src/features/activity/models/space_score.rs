5a620daaf5377241a377e8b0173d62482f11bc28
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2026-04-09 15:59:33 +0900

    fix ranking logic and add migrate code

--- app/ratel/src/features/activity/models/space_score.rs
+++ app/ratel/src/features/activity/models/space_score.rs
@@ -6,11 +6,17 @@ pub struct SpaceScore {
     pub pk: CompositePartition<SpacePartition, AuthorPartition>,
     pub sk: EntityType,
 
+    // FIXME: remove gsi1 field when space score table is migrated, currently it is used for querying scores by space, but it is redundant since we can query by space_pk
     #[dynamo(prefix = "SCSP", name = "find_by_space", index = "gsi1", pk)]
+    #[dynamo(prefix = "SCSP", name = "find_by_space_rank", index = "gsi2", pk)]
     pub space_pk: Partition,
     #[dynamo(prefix = "SCR", index = "gsi1", sk)]
     pub total_score: i64,
+    #[serde(default)]
+    #[dynamo(prefix = "SCR", index = "gsi2", order = 1, sk)]
+    pub rank_total_score: i64,
 
+    #[dynamo(index = "gsi2", order = 3, sk)]
     pub user_pk: AuthorPartition,
     pub user_name: String,
     pub user_avatar: String,
@@ -20,6 +26,7 @@ pub struct SpaceScore {
     pub follow_score: i64,
     pub discussion_score: i64,
 
+    #[dynamo(index = "gsi2", order = 2, sk)]
     pub updated_at: i64,
 }
 
@@ -39,6 +46,7 @@ impl SpaceScore {
             sk: EntityType::SpaceScore,
             space_pk,
             total_score: 0,
+            rank_total_score: 0,
             user_pk: author,
             user_name,
             user_avatar,
@@ -53,7 +61,10 @@ impl SpaceScore {
     pub fn keys(
         space_id: &SpacePartition,
         author: &AuthorPartition,
-    ) -> (CompositePartition<SpacePartition, AuthorPartition>, EntityType) {
+    ) -> (
+        CompositePartition<SpacePartition, AuthorPartition>,
+        EntityType,
+    ) {
         (
             CompositePartition(space_id.clone(), author.clone()),
             EntityType::SpaceScore,
