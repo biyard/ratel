a17af8057a7de2a2efa4aec68cb4a3dff568f1b0
leechanhui <leechanhui@leechanhuiui-MacBookPro.local>
2026-04-12 17:47:34 +0900

    fix setting button location

--- app/ratel/src/features/spaces/pages/actions/actions/poll/views/main/mod.rs
+++ app/ratel/src/features/spaces/pages/actions/actions/poll/views/main/mod.rs
@@ -56,7 +56,7 @@ pub fn PollActionPage(space_id: SpacePartition, poll_id: SpacePollEntityType) ->
     };
 
     rsx! {
-        div { class: "flex flex-col flex-1 mx-auto w-full min-h-0",
+        div { class: "flex flex-col flex-1 mx-auto w-full min-h-0 max-w-desktop",
             if !show_creator_view {
                 SettingsSwitchButton {}
             }
